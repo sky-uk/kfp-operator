fd515375ff5d95f7e22ebd081344c2c57172479a
Jonas Mende <jonas.mende@sky.uk>
2021-09-27 11:42:10 +0100

    Use default version of 0.0.0

--- docker.mk
+++ docker.mk
@@ -1,13 +1,13 @@
-VERSION := $(shell git describe --tags --match 'v[0-9]*\.[0-9]*\.[0-9]*')
+VERSION := $(shell git describe --tags --match 'v[0-9]*\.[0-9]*\.[0-9]*' || echo 0.0.0)
 
 ##@ Container
 
 ifdef CONTAINER_REGISTRY_HOSTS
-PUSH_TARGETS := $(foreach h,$(CONTAINER_REGISTRY_HOSTS),push_to_$(h))
-docker-push: docker-build $(PUSH_TARGETS) ## Push container image
+PUSH_TARGETS := $(foreach h,${CONTAINER_REGISTRY_HOSTS},push_to_${h})
+docker-push: docker-build ${PUSH_TARGETS} ## Push container image
 $(PUSH_TARGETS):
-	docker tag ${IMG} $@${IMG}:$(VERSION)
-	docker push $@${IMG}:$(VERSION)
+	docker tag ${IMG} $@$/${IMG}:${VERSION}
+	docker push $@/${IMG}:${VERSION}
 else
 docker-push:
 	$(error CONTAINER_REGISTRY_HOSTS must be a space-separated list of hosts)
