09eeb90630c07c071541c10ee516afa7483d2e4b
Jonas Mende <jonas.mende@sky.uk>
2021-09-27 11:34:56 +0100

    Add host to tag

--- docker.mk
+++ docker.mk
@@ -6,8 +6,8 @@ ifdef CONTAINER_REGISTRY_HOSTS
 PUSH_TARGETS := $(foreach h,$(CONTAINER_REGISTRY_HOSTS),push_to_$(h))
 docker-push: docker-build $(PUSH_TARGETS) ## Push container image
 $(PUSH_TARGETS):
-	docker tag ${IMG} ${IMG}:$(VERSION)
-	docker push ${IMG}:$(VERSION)
+	docker tag ${IMG} $@${IMG}:$(VERSION)
+	docker push $@${IMG}:$(VERSION)
 else
 docker-push:
 	$(error CONTAINER_REGISTRY_HOSTS must be a space-separated list of hosts)
