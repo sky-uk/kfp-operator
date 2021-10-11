029d8a4b04ef7b0b3e507547ec9f71d39a742776
Paolo Ambrosio - Sky UK <paolo.ambrosio2@sky.uk>
2021-10-11 10:43:21 +0100

    Fixed container version (#26)
    Added dirty flag to versions as well

---
+++ docker-targets.mk
@@ -0,0 +1,15 @@
+ifndef CONTAINER_REGISTRY_HOSTS
+docker-push:
+	$(error CONTAINER_REGISTRY_HOSTS must be a space-separated list of hosts)
+else ifneq ($(VERSION), $(VERSION:-dirty=))
+docker-push:
+	$(error Refusing to push dirty image $(VERSION))
+else
+docker-push: docker-build $(CONTAINER_REGISTRY_HOSTS) ## Push container image
+$(CONTAINER_REGISTRY_HOSTS):
+	docker tag ${IMG}:${VERSION} $@/${IMG}:${VERSION}
+	docker push $@/${IMG}:${VERSION}
+endif
+
+docker-build: build ## Build container image
+	docker build -t ${IMG} -t ${IMG}:${VERSION} .
