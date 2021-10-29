ba37ed25558d474538152515fc168696ef574453
Jonas Mende <jonas.mende@sky.uk>
2021-10-29 13:31:15 +0100

    Empty lines

--- argo/mlmd-cli/mlmd_cli/cli.py
+++ argo/mlmd-cli/mlmd_cli/cli.py
@@ -24,16 +24,11 @@ def get(ctx, workflow_name, pipeline_name):
   client = ctx.obj['client']
 
   pipeline_id = client.get_context_id(context_type='pipeline', context_name=pipeline_name)
-  
   execution_id = client.get_execution_id(context_id=pipeline_id, run_id=workflow_name, component_id='Pusher')
-
   artifact_id = client.get_artifact_id(execution_id=execution_id, step_name='pushed_model')
-
   destination = client.get_artifact_property(artifact_id, property_name='pushed_destination')
 
   click.echo(destination)
-
-
 
 def main():
     cli.add_command(pushed_model_artifact)
