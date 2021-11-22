a9ecd978df3e30755f87314ae000b30649b2a981
Jonas Mende <jonas.mende@sky.uk>
2021-11-22 14:50:36 +0000

    Invert order of state transition commands

--- controllers/pipelines/test_context.go
+++ controllers/pipelines/test_context.go
@@ -6,6 +6,7 @@ package pipelines
 import (
 	"context"
 	"errors"
+	"fmt"
 	argo "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/types"
@@ -69,6 +70,7 @@ func (testCtx TestContext) WorkflowByOperationToMatch(operation string, matcher 
 }
 
 func (testCtx TestContext) UpdateWorkflow(operation string, updateFunc func(*argo.Workflow)) error {
+	fmt.Println("Checking")
 	workflow, err := testCtx.fetchWorkflow(operation)
 
 	if err != nil {
@@ -77,6 +79,12 @@ func (testCtx TestContext) UpdateWorkflow(operation string, updateFunc func(*arg
 
 	updateFunc(workflow)
 	return testCtx.K8sClient.Update(testCtx.ctx, workflow)
+}
+
+func (testCtx TestContext) WorkflowToBeUpdated(operation string, updateFunc func(*argo.Workflow)) func(g Gomega) {
+	return func(g Gomega) {
+		g.Expect(testCtx.UpdateWorkflow(operation, updateFunc)).To(Succeed())
+	}
 }
 
 func (testCtx TestContext) FetchWorkflow(operation string) func() error {
@@ -93,8 +101,9 @@ func (testCtx TestContext) fetchWorkflow(operation string) (*argo.Workflow, erro
 		return nil, err
 	}
 
-	if len(workflowList.Items) != 1 {
-		return nil, errors.New("not exactly one workflow")
+	numberOfWorkflows := len(workflowList.Items)
+	if numberOfWorkflows != 1 {
+		return nil, errors.New(fmt.Sprintf("Want exactly 1 workflow. Have %d", numberOfWorkflows))
 	}
 
 	return &workflowList.Items[0], nil
