be97d4fa65d99f10e70234b8596cdcee2ce97470
Jonas Mende <jonas.mende@sky.uk>
2022-06-23 12:02:30 +0100

    Replace go-get with go-install
    go-get got deprecated in go 1.18: https://go.dev/doc/go-get-install-deprecation#:~:text=Starting%20in%20Go%201.17%2C%20installing,the%20%2Dd%20flag%20were%20enabled.

--- go-get.mk
+++ go-get.mk
@@ -1,19 +1,19 @@
-ifndef go-get-tool
+ifndef go-install
 
-# go-get-tool will 'go get' any package $2 and install it to $1.
+# go-install will 'go install' any package $2 and install it to $1.
 PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
 export PATH := $(PATH):$(PROJECT_DIR)/bin
 
-define go-get-tool
+define go-install
 @[ -f $(1) ] || { \
 set -e ;\
 TMP_DIR=$$(mktemp -d) ;\
 cd $$TMP_DIR ;\
 go mod init tmp ;\
 echo "Downloading $(2)" ;\
-GOBIN=$(PROJECT_DIR)/bin go get $(2) ;\
-rm -rf $$TMP_DIR ;\
+GOBIN=$(PROJECT_DIR)/bin go install $(2) ;\
+echo $$TMP_DIR ;\
 }
 endef
 
-endif # go-get-tool
+endif # go-install
