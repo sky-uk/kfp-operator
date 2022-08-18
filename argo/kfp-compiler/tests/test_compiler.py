7a85555c5b1733cf32d7989d8c096b9a08177cd5
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2022-08-18 16:00:32 -0400

    add capability of compiler to parse beam args with list values

--- argo/kfp-compiler/tests/test_compiler.py
+++ argo/kfp-compiler/tests/test_compiler.py
@@ -3,10 +3,13 @@ from kfp_compiler import compiler
 def test_dict_to_cli_args():
     args = {
         'a': 'aVal',
-        'b': 'bVal'
+        'b': 'bVal',
+        'c': ['cVal1', 'cVal2'],
     }
 
     assert compiler.dict_to_cli_args(args) == [
         '--a=aVal',
-        '--b=bVal'
+        '--b=bVal',
+        '--c=cVal1',
+        '--c=cVal2',
     ]
