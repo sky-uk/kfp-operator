805e6bdcd97c92e91d386014c1b2773e7c34bbfc
Jonas Mende <jonas.mende@sky.uk>
2022-08-25 14:21:28 +0100

    Implementation

--- apis/pipelines/v1alpha2/pipeline_conversion.go
+++ apis/pipelines/v1alpha2/pipeline_conversion.go
@@ -1,7 +1,75 @@
 package v1alpha2
 
-import "sigs.k8s.io/controller-runtime/pkg/conversion"
+import (
+	"fmt"
+	"github.com/sky-uk/kfp-operator/apis/pipelines/v1alpha3"
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+	"sort"
+)
 
 func (src *Pipeline) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*v1alpha3.Pipeline)
+
+	dst.Spec.Env = mapToNamedValues(src.Spec.Env)
+	dst.Spec.BeamArgs = mapToNamedValues(src.Spec.BeamArgs)
+	dst.Spec.Image = src.Spec.Image
+	dst.Spec.TfxComponents = src.Spec.TfxComponents
+
 	return nil
+}
+
+func (dst *Pipeline) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*v1alpha3.Pipeline)
+
+	var err error
+
+	dst.Spec.Env, err = namedValuesToMap(src.Spec.Env)
+	if err != nil {
+		return err
+	}
+	dst.Spec.BeamArgs, err = namedValuesToMap(src.Spec.BeamArgs)
+	if err != nil {
+		return err
+	}
+	dst.Spec.Image = src.Spec.Image
+	dst.Spec.TfxComponents = src.Spec.TfxComponents
+	return nil
+}
+
+func namedValuesToMap(namedValues []v1alpha3.NamedValue) (map[string]string, error) {
+	if len(namedValues) == 0 {
+		return nil, nil
+	}
+
+	values := make(map[string]string, len(namedValues))
+
+	for _, nv := range namedValues {
+		if _, exists := values[nv.Name]; exists {
+			return nil, fmt.Errorf("duplicate entry: %s", nv.Name)
+		}
+
+		values[nv.Name] = nv.Value
+	}
+
+	return values, nil
+}
+
+func mapToNamedValues(values map[string]string) []v1alpha3.NamedValue {
+	var namedValues []v1alpha3.NamedValue
+
+	for k, v := range values {
+		namedValues = append(namedValues, v1alpha3.NamedValue{
+			Name: k, Value: v,
+		})
+	}
+
+	sort.Slice(namedValues, func(i, j int) bool {
+		if namedValues[i].Name != namedValues[j].Name {
+			return namedValues[i].Name < namedValues[j].Name
+		} else {
+			return namedValues[i].Value < namedValues[j].Value
+		}
+	})
+
+	return namedValues
 }
--- apis/pipelines/v1alpha2/pipeline_conversion_test.go
+++ apis/pipelines/v1alpha2/pipeline_conversion_test.go
@@ -8,7 +8,6 @@ import (
 
 var _ = Context("Pipeline Conversion", func() {
 	var _ = Describe("ConvertTo", func() {
-
 		Specify("Converts Env to a list of NamedValue", func() {
 			src := Pipeline{Spec: PipelineSpec{Env: map[string]string{"a": "b", "c": "d"}}}
 			expected := v1alpha3.Pipeline{Spec: v1alpha3.PipelineSpec{Env: []v1alpha3.NamedValue{
@@ -21,5 +20,94 @@ var _ = Context("Pipeline Conversion", func() {
 			Expect(dst).To(Equal(expected))
 		})
 
+		Specify("Converts BeamArgs to a list of NamedValue", func() {
+			src := Pipeline{Spec: PipelineSpec{BeamArgs: map[string]string{"a": "b", "c": "d"}}}
+			expected := v1alpha3.Pipeline{Spec: v1alpha3.PipelineSpec{BeamArgs: []v1alpha3.NamedValue{
+				{Name: "a", Value: "b"},
+				{Name: "c", Value: "d"},
+			}}}
+
+			dst := v1alpha3.Pipeline{}
+			Expect(src.ConvertTo(&dst)).To(Succeed())
+			Expect(dst).To(Equal(expected))
+		})
+
+		Specify("Copies all other fields", func() {
+			src := Pipeline{
+				Spec: PipelineSpec{Image: "image", TfxComponents: "components"},
+			}
+
+			dst := v1alpha3.Pipeline{}
+			Expect(src.ConvertTo(&dst)).To(Succeed())
+			Expect(dst.Spec.Image).To(Equal(src.Spec.Image))
+			Expect(dst.Spec.TfxComponents).To(Equal(src.Spec.TfxComponents))
+		})
+	})
+
+	var _ = Describe("ConvertFrom", func() {
+		Specify("Converts Env to a map", func() {
+			src := v1alpha3.Pipeline{Spec: v1alpha3.PipelineSpec{Env: []v1alpha3.NamedValue{
+				{Name: "a", Value: "b"},
+				{Name: "c", Value: "d"},
+			}}}
+			expected := Pipeline{Spec: PipelineSpec{Env: map[string]string{"a": "b", "c": "d"}}}
+			dst := Pipeline{}
+
+			Expect(dst.ConvertFrom(&src)).To(Succeed())
+			Expect(dst).To(Equal(expected))
+		})
+
+		Specify("Errors when Env contains a duplicate NamedValue", func() {
+			src := v1alpha3.Pipeline{Spec: v1alpha3.PipelineSpec{Env: []v1alpha3.NamedValue{
+				{Name: "a", Value: "b"},
+				{Name: "a", Value: "d"},
+			}}}
+			dst := Pipeline{}
+
+			Expect(dst.ConvertFrom(&src)).NotTo(Succeed())
+		})
+
+		Specify("Converts BeamArgs to a map", func() {
+			src := v1alpha3.Pipeline{Spec: v1alpha3.PipelineSpec{BeamArgs: []v1alpha3.NamedValue{
+				{Name: "a", Value: "b"},
+				{Name: "c", Value: "d"},
+			}}}
+			expected := Pipeline{Spec: PipelineSpec{BeamArgs: map[string]string{"a": "b", "c": "d"}}}
+			dst := Pipeline{}
+
+			Expect(dst.ConvertFrom(&src)).To(Succeed())
+			Expect(dst).To(Equal(expected))
+		})
+
+		Specify("Errors when BeamArgs contains a duplicate NamedValue", func() {
+			src := v1alpha3.Pipeline{Spec: v1alpha3.PipelineSpec{BeamArgs: []v1alpha3.NamedValue{
+				{Name: "a", Value: "b"},
+				{Name: "a", Value: "d"},
+			}}}
+			dst := Pipeline{}
+
+			Expect(dst.ConvertFrom(&src)).NotTo(Succeed())
+		})
+
+		Specify("Copies all other fields", func() {
+			src := v1alpha3.Pipeline{Spec: v1alpha3.PipelineSpec{Image: "image", TfxComponents: "components"}}
+			expected := Pipeline{Spec: PipelineSpec{Image: "image", TfxComponents: "components"}}
+			dst := Pipeline{}
+
+			Expect(dst.ConvertFrom(&src)).To(Succeed())
+			Expect(dst).To(Equal(expected))
+		})
+	})
+
+	var _ = Describe("ComputeVersion", func() {
+		Specify("Does not change between versions", func() {
+			src := Pipeline{
+				Spec: PipelineSpec{Image: "image", TfxComponents: "components", Env: map[string]string{"a": "b", "c": "d"}, BeamArgs: map[string]string{"a": "b", "c": "d"}},
+			}
+
+			dst := v1alpha3.Pipeline{}
+			Expect(src.ConvertTo(&dst)).To(Succeed())
+			Expect(src.Spec.ComputeVersion()).To(Equal(dst.Spec.ComputeVersion()))
+		})
 	})
 })
