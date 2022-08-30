d386d6ab16e871ebc315c3fbd4a33802bde1627a
Jonas Mende <jonas.mende@sky.uk>
2022-08-30 12:52:11 +0100

    Make compiler accept lists only

--- argo/kfp-compiler/tests/test_compiler.py
+++ argo/kfp-compiler/tests/test_compiler.py
@@ -2,8 +2,8 @@ from kfp_compiler import compiler
 
 def test_dict_to_cli_args():
     args = {
-        'a': 'aVal',
-        'b': 'bVal',
+        'a': ['aVal'],
+        'b': ['bVal'],
         'c': ['cVal1', 'cVal2'],
     }
 
