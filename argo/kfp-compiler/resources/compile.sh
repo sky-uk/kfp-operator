8222d13623c61a698e04546601c23e1180ced93c
jmendesky <jonas.mende@sky.uk>
2022-09-09 10:36:28 +0100

    Specify python major version

--- argo/kfp-compiler/resources/compile.sh
+++ argo/kfp-compiler/resources/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PYTHON_VERSION=$(python -V | grep -Eo '[0-9]+\.[0-9]+')
+PYTHON_VERSION=$(python3 -V | grep -Eo '[0-9]+\.[0-9]+')
 
 export PYTHONPATH="$PYTHONPATH:/$(dirname $0)/py$PYTHON_VERSION"
 
