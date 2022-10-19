3d6ab8b517c9ae3dd2cbf51ec0818256f6cd2ae5
Jonas Mende <jonas.mende@sky.uk>
2022-10-19 15:49:50 +0100

    Clean up temp files after running tests

--- argo/kfp-compiler/acceptance/test_compiler.py
+++ argo/kfp-compiler/acceptance/test_compiler.py
@@ -4,36 +4,37 @@ import yaml
 import json
 import os
 import sys
-import tempfile
-dirpath = tempfile.mkdtemp()
+from tempfile import TemporaryDirectory
 
 runner = CliRunner()
 config_file_path = 'acceptance/pipeline_conf.yaml'
 
 def test_cli_v1():
-    output_file_path = dirpath+'/pipeline.yaml'
+    with TemporaryDirectory() as tmp_dir:
+        output_file_path = os.path.join(tmp_dir, 'pipeline.yaml')
 
-    sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "..", "docs-gen", "includes", "quickstart", "penguin_pipeline"))
-    result = runner.invoke(compiler.compile, ['--pipeline_config', config_file_path, '--output_file', output_file_path, "--execution_mode", "v1"])
+        sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "..", "docs-gen", "includes", "quickstart", "penguin_pipeline"))
+        result = runner.invoke(compiler.compile, ['--pipeline_config', config_file_path, '--output_file', output_file_path, "--execution_mode", "v1"])
 
-    assert result.exit_code == 0
-    assert os.stat(output_file_path).st_size != 0
+        assert result.exit_code == 0
+        assert os.stat(output_file_path).st_size != 0
 
-    f = open(output_file_path, "r")
-    workflow = yaml.load(f.read())
-    assert workflow['kind'] == 'Workflow'
+        f = open(output_file_path, "r")
+        workflow = yaml.load(f.read())
+        assert workflow['kind'] == 'Workflow'
 
 def test_cli_v2():
-    output_file_path = dirpath+'/pipeline.json'
-    sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "..", "docs-gen", "includes", "quickstart", "penguin_pipeline"))
-    result = runner.invoke(compiler.compile, ['--pipeline_config', config_file_path, '--output_file', output_file_path, "--execution_mode", "v2"])
+    with TemporaryDirectory() as tmp_dir:
+        output_file_path = os.path.join(tmp_dir, 'pipeline.json')
+        sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "..", "docs-gen", "includes", "quickstart", "penguin_pipeline"))
+        result = runner.invoke(compiler.compile, ['--pipeline_config', config_file_path, '--output_file', output_file_path, "--execution_mode", "v2"])
 
-    assert result.exit_code == 0
-    assert os.stat(output_file_path).st_size != 0
+        assert result.exit_code == 0
+        assert os.stat(output_file_path).st_size != 0
 
-    f = open(output_file_path, "r")
-    pipeline = json.loads(f.read())
-    assert pipeline['pipelineSpec']['schemaVersion'] == '2.0.0'
+        f = open(output_file_path, "r")
+        pipeline = json.loads(f.read())
+        assert pipeline['pipelineSpec']['schemaVersion'] == '2.0.0'
 
 
 def test_failure():
