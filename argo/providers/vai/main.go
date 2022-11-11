afe7eaced6e55d826738d47707b0b55ac31eaf5f
Jonas Mende <jonas.mende@sky.uk>
2022-11-11 11:33:42 +0000

    Add custom command to enqueue vai run
    Co-authored-by: Paolo Ambrosio <paolo.ambrosio2@sky.uk>

--- argo/providers/vai/main.go
+++ argo/providers/vai/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"cloud.google.com/go/pubsub"
 	"cloud.google.com/go/scheduler/apiv1"
 	"cloud.google.com/go/storage"
 	"context"
@@ -9,6 +10,7 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	. "github.com/sky-uk/kfp-operator/providers/base"
+	"github.com/urfave/cli"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 	schedulerpb "google.golang.org/genproto/googleapis/cloud/scheduler/v1"
@@ -17,35 +19,93 @@ import (
 	"os"
 )
 
-func main() {
-	RunProviderApp[VertexAiProviderConfig](VAIProvider{})
+var labels = struct {
+	RunConfiguration string
+	PipelineName     string
+	PipelineVersion  string
+}{
+	RunConfiguration: "run-configuration",
+	PipelineName:     "pipeline-name",
+	PipelineVersion:  "pipeline-version",
 }
 
-type VertexAiProviderConfig struct {
-	PipelineBucket  string `yaml:"pipelineBucket,omitempty"`
-	Endpoint        string `yaml:"endpoint,omitempty"`
-	RunIntentsTopic string `yaml:"runIntentsTopic,omitempty"`
-	Project         string `yaml:"project,omitempty"`
-	Location        string `yaml:"location,omitempty"`
+func main() {
+	app := NewProviderApp[VAIProviderConfig]()
+	provider := VAIProvider{}
+	app.Run(provider, cli.Command{
+		Name: "vai-run",
+		Subcommands: []cli.Command{
+			{
+				Name: "enqueue",
+				Flags: []cli.Flag{cli.StringFlag{
+					Name:     "run-intent",
+					Required: true,
+				}},
+				Action: func(c *cli.Context) error {
+					providerConfig, err := app.LoadProviderConfig(c)
+					if err != nil {
+						return err
+					}
+					runIntent, err := LoadJsonFromFile[RunIntent](c.String("run-intent"))
+					if err != nil {
+						return err
+					}
+					return provider.enqueueRun(app.Context, providerConfig, runIntent)
+				},
+			},
+			{
+				Name: "submit",
+				Flags: []cli.Flag{cli.StringFlag{
+					Name:     "run",
+					Required: true,
+				}},
+				Action: func(c *cli.Context) error {
+					providerConfig, err := app.LoadProviderConfig(c)
+					vaiRun, err := LoadYamlFromFile[VAIRun](c.String("run"))
+					if err != nil {
+						return err
+					}
+					return provider.submitRun(app.Context, providerConfig, vaiRun)
+				},
+			},
+		},
+	})
+}
+
+type VAIProviderConfig struct {
+	PipelineBucket  string `yaml:"pipelineBucket"`
+	Endpoint        string `yaml:"endpoint"`
+	RunIntentsTopic string `yaml:"runIntentsTopic"`
+	Project         string `yaml:"project"`
+	Location        string `yaml:"location"`
+	RunsTopic       string `yaml:"runsTopic"`
 }
 
 type VAIProvider struct {
 }
 
-func (vaip VAIProvider) parent(providerConfig VertexAiProviderConfig) string {
+func (vaip VAIProvider) parent(providerConfig VAIProviderConfig) string {
 	//TODO load from default?
 	return fmt.Sprintf(`projects/%s/locations/%s`, providerConfig.Project, providerConfig.Location)
 }
 
-func (vaip VAIProvider) jobName(providerConfig VertexAiProviderConfig, name string) string {
+func (vaip VAIProvider) jobName(providerConfig VAIProviderConfig, name string) string {
 	return fmt.Sprintf("%s/jobs/%s", vaip.parent(providerConfig), name)
 }
 
-func (vaip VAIProvider) topic(providerConfig VertexAiProviderConfig, topicName string) string {
+func (vaip VAIProvider) topic(providerConfig VAIProviderConfig, topicName string) string {
 	return fmt.Sprintf("projects/%s/topics/%s", providerConfig.Project, topicName)
 }
 
-func (vaip VAIProvider) gcsClient(ctx context.Context, providerConfig VertexAiProviderConfig) (*storage.Client, error) {
+func (vaip VAIProvider) pipelineStorageObject(pipelineName string, pipelineVersion string) string {
+	return fmt.Sprintf("%s/%s", pipelineName, pipelineVersion)
+}
+
+func (vaip VAIProvider) pipelineUri(providerConfig VAIProviderConfig, pipelineName string, pipelineVersion string) string {
+	return fmt.Sprintf("gs://%s/%s", providerConfig.PipelineBucket, vaip.pipelineStorageObject(pipelineName, pipelineVersion))
+}
+
+func (vaip VAIProvider) gcsClient(ctx context.Context, providerConfig VAIProviderConfig) (*storage.Client, error) {
 	var client *storage.Client
 	var err error
 	if providerConfig.Endpoint != "" {
@@ -57,20 +117,15 @@ func (vaip VAIProvider) gcsClient(ctx context.Context, providerConfig VertexAiPr
 	return client, err
 }
 
-func (vaip VAIProvider) CreatePipeline(ctx context.Context, providerConfig VertexAiProviderConfig, pipelineDefinition PipelineDefinition, pipelineFile string) (string, error) {
-	id, err := uuid.NewUUID()
-	if err != nil {
+func (vaip VAIProvider) CreatePipeline(ctx context.Context, providerConfig VAIProviderConfig, pipelineDefinition PipelineDefinition, pipelineFile string) (string, error) {
+	if _, err := vaip.UpdatePipeline(ctx, providerConfig, pipelineDefinition, pipelineDefinition.Name, pipelineFile); err != nil {
 		return "", err
 	}
 
-	if _, err := vaip.UpdatePipeline(ctx, providerConfig, pipelineDefinition, id.String(), pipelineFile); err != nil {
-		return "", err
-	}
-
-	return id.String(), nil
+	return pipelineDefinition.Name, nil
 }
 
-func (vaip VAIProvider) UpdatePipeline(ctx context.Context, providerConfig VertexAiProviderConfig, pipelineDefinition PipelineDefinition, id string, pipelineFile string) (string, error) {
+func (vaip VAIProvider) UpdatePipeline(ctx context.Context, providerConfig VAIProviderConfig, pipelineDefinition PipelineDefinition, id string, pipelineFile string) (string, error) {
 	client, err := vaip.gcsClient(ctx, providerConfig)
 	if err != nil {
 		return id, err
@@ -81,7 +136,7 @@ func (vaip VAIProvider) UpdatePipeline(ctx context.Context, providerConfig Verte
 		return id, err
 	}
 
-	writer := client.Bucket(providerConfig.PipelineBucket).Object(fmt.Sprintf("%s/%s", id, pipelineDefinition.Version)).NewWriter(ctx)
+	writer := client.Bucket(providerConfig.PipelineBucket).Object(vaip.pipelineStorageObject(id, pipelineDefinition.Version)).NewWriter(ctx)
 	_, err = io.Copy(writer, reader)
 	if err != nil {
 		return id, err
@@ -100,7 +155,7 @@ func (vaip VAIProvider) UpdatePipeline(ctx context.Context, providerConfig Verte
 	return id, nil
 }
 
-func (vaip VAIProvider) DeletePipeline(ctx context.Context, providerConfig VertexAiProviderConfig, id string) error {
+func (vaip VAIProvider) DeletePipeline(ctx context.Context, providerConfig VAIProviderConfig, id string) error {
 	client, err := vaip.gcsClient(ctx, providerConfig)
 	if err != nil {
 		return err
@@ -127,14 +182,21 @@ func (vaip VAIProvider) DeletePipeline(ctx context.Context, providerConfig Verte
 	return nil
 }
 
+type VAIRun struct {
+	RunId             string            `json:"runId"`
+	Labels            map[string]string `json:"labels,omitempty"`
+	PipelineUri       string            `json:"pipelineUri"`
+	RuntimeParameters map[string]string `json:"runtimeParameters,omitempty"`
+}
+
 type RunIntent struct {
 	RunConfigurationName string            `json:"runConfigurationName,omitempty"`
-	PipelineName         string            `json:"pipelineName,omitempty"`
-	PipelineVersion      string            `json:"pipelineVersion,omitempty"`
+	PipelineName         string            `json:"pipelineName"`
+	PipelineVersion      string            `json:"pipelineVersion"`
 	RuntimeParameters    map[string]string `json:"runtimeParameters,omitempty"`
 }
 
-func (vaip VAIProvider) createSchedulerJobPb(providerConfig VertexAiProviderConfig, runConfigurationDefinition RunConfigurationDefinition) (*schedulerpb.Job, error) {
+func (vaip VAIProvider) createSchedulerJobPb(providerConfig VAIProviderConfig, runConfigurationDefinition RunConfigurationDefinition) (*schedulerpb.Job, error) {
 	fmt.Println(runConfigurationDefinition.Schedule)
 	schedule, err := ParseCron(runConfigurationDefinition.Schedule)
 	if err != nil {
@@ -165,7 +227,7 @@ func (vaip VAIProvider) createSchedulerJobPb(providerConfig VertexAiProviderConf
 	}, nil
 }
 
-func (vaip VAIProvider) CreateRunConfiguration(ctx context.Context, providerConfig VertexAiProviderConfig, runConfigurationDefinition RunConfigurationDefinition) (string, error) {
+func (vaip VAIProvider) CreateRunConfiguration(ctx context.Context, providerConfig VAIProviderConfig, runConfigurationDefinition RunConfigurationDefinition) (string, error) {
 	client, err := scheduler.NewCloudSchedulerClient(ctx)
 	if err != nil {
 		return "", err
@@ -188,7 +250,7 @@ func (vaip VAIProvider) CreateRunConfiguration(ctx context.Context, providerConf
 	return job.Name, nil
 }
 
-func (vaip VAIProvider) UpdateRunConfiguration(ctx context.Context, providerConfig VertexAiProviderConfig, runConfigurationDefinition RunConfigurationDefinition, providerId string) (string, error) {
+func (vaip VAIProvider) UpdateRunConfiguration(ctx context.Context, providerConfig VAIProviderConfig, runConfigurationDefinition RunConfigurationDefinition, providerId string) (string, error) {
 	client, err := scheduler.NewCloudSchedulerClient(ctx)
 	if err != nil {
 		return "", err
@@ -218,7 +280,7 @@ func (vaip VAIProvider) UpdateRunConfiguration(ctx context.Context, providerConf
 	return job.Name, nil
 }
 
-func (vaip VAIProvider) DeleteRunConfiguration(ctx context.Context, _ VertexAiProviderConfig, providerId string) error {
+func (vaip VAIProvider) DeleteRunConfiguration(ctx context.Context, _ VAIProviderConfig, providerId string) error {
 	client, err := scheduler.NewCloudSchedulerClient(ctx)
 	if err != nil {
 		return err
@@ -229,14 +291,47 @@ func (vaip VAIProvider) DeleteRunConfiguration(ctx context.Context, _ VertexAiPr
 	})
 }
 
-func (vaip VAIProvider) CreateExperiment(_ context.Context, _ VertexAiProviderConfig, _ ExperimentDefinition) (string, error) {
+func (vaip VAIProvider) CreateExperiment(_ context.Context, _ VAIProviderConfig, _ ExperimentDefinition) (string, error) {
 	return "", errors.New("not implemented")
 }
 
-func (vaip VAIProvider) UpdateExperiment(_ context.Context, _ VertexAiProviderConfig, _ ExperimentDefinition, _ string) (string, error) {
+func (vaip VAIProvider) UpdateExperiment(_ context.Context, _ VAIProviderConfig, _ ExperimentDefinition, _ string) (string, error) {
 	return "", errors.New("not implemented")
 }
 
-func (vaip VAIProvider) DeleteExperiment(_ context.Context, _ VertexAiProviderConfig, _ string) error {
+func (vaip VAIProvider) DeleteExperiment(_ context.Context, _ VAIProviderConfig, _ string) error {
 	return errors.New("not implemented")
+}
+
+func (vaip VAIProvider) enqueueRun(ctx context.Context, providerConfig VAIProviderConfig, runIntent RunIntent) error {
+	pubsubClient, err := pubsub.NewClient(ctx, providerConfig.Project)
+	if err != nil {
+		return err
+	}
+
+	topic := pubsubClient.Topic(providerConfig.RunsTopic)
+	defer topic.Stop()
+
+	vaiRun := VAIRun{
+		RunId:       fmt.Sprintf("rc-%s-%s", runIntent.RunConfigurationName, uuid.New().String()),
+		PipelineUri: vaip.pipelineUri(providerConfig, runIntent.PipelineName, runIntent.PipelineVersion),
+		Labels: map[string]string{
+			labels.RunConfiguration: runIntent.RunConfigurationName,
+			labels.PipelineName:     runIntent.PipelineName,
+			labels.PipelineVersion:  runIntent.PipelineVersion,
+		},
+		RuntimeParameters: runIntent.RuntimeParameters,
+	}
+
+	payload, err := json.Marshal(vaiRun)
+	if err != nil {
+		return err
+	}
+
+	_, err = topic.Publish(ctx, &pubsub.Message{Data: payload}).Get(ctx)
+	return err
+}
+
+func (vaip VAIProvider) submitRun(ctx context.Context, providerConfig VAIProviderConfig, vaiRun VAIRun) error {
+	return nil
 }
