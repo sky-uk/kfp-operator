919d563178b058c32b1bb2d1bce11f7daf2dff5b
Jonas Mende <jonas.mende@sky.uk>
2022-11-23 17:03:33 +0000

    Don't fetch resource for reconciler request
    Co-authored-by: Paolo Ambrosio <paolo.ambrosio2@sky.uk>

--- controllers/pipelines/reconciler.go
+++ controllers/pipelines/reconciler.go
@@ -1,7 +1,6 @@
 package pipelines
 
 import (
-	"context"
 	pipelinesv1 "github.com/sky-uk/kfp-operator/apis/pipelines/v1alpha4"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -15,21 +14,14 @@ type BaseReconciler[R pipelinesv1.Resource] struct {
 
 func (br BaseReconciler[R]) reconciliationRequestsWorkflow(workflow client.Object, resource R) []reconcile.Request {
 	kind, hasKind := workflow.GetLabels()[WorkflowConstants.OwnerKindLabelKey]
-	ownerName, hasOnwerName := workflow.GetLabels()[WorkflowConstants.OwnerNameLabelKey]
-	ownerNamespace, hasOnwerNamespace := workflow.GetLabels()[WorkflowConstants.OwnerNamespaceLabelKey]
-	if !hasKind || !hasOnwerName || !hasOnwerNamespace || kind != resource.GetKind() {
+	ownerName, hasOwnerName := workflow.GetLabels()[WorkflowConstants.OwnerNameLabelKey]
+	ownerNamespace, hasOwnerNamespace := workflow.GetLabels()[WorkflowConstants.OwnerNamespaceLabelKey]
+	if !hasKind || !hasOwnerName || !hasOwnerNamespace || kind != resource.GetKind() {
 		return nil
 	}
 
-	namespacedName := types.NamespacedName{
+	return []reconcile.Request{{NamespacedName: types.NamespacedName{
 		Name:      ownerName,
 		Namespace: ownerNamespace,
-	}
-
-	err := br.EC.Client.Cached.Get(context.TODO(), namespacedName, resource)
-	if err != nil {
-		return []reconcile.Request{}
-	}
-
-	return []reconcile.Request{{NamespacedName: namespacedName}}
+	}}}
 }
