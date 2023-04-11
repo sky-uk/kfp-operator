7ec7f84b905e3ad1a0e3c8a1e9d1d0529fa25249
Jonas Mende <jonas.mende@sky.uk>
2023-04-11 13:45:18 +0100

    Use random resources

--- controllers/pipelines/runconfiguration_workflow_integration_test.go
+++ controllers/pipelines/runconfiguration_workflow_integration_test.go
@@ -5,10 +5,8 @@ package pipelines
 
 import (
 	. "github.com/onsi/ginkgo/v2"
-	"github.com/sky-uk/kfp-operator/apis"
 	config "github.com/sky-uk/kfp-operator/apis/config/v1alpha4"
 	pipelinesv1 "github.com/sky-uk/kfp-operator/apis/pipelines/v1alpha4"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Context("Resource Workflows", Serial, func() {
@@ -20,21 +18,7 @@ var _ = Context("Resource Workflows", Serial, func() {
 	})
 
 	var newRunConfiguration = func() *pipelinesv1.RunConfiguration {
-		return &pipelinesv1.RunConfiguration{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      apis.RandomLowercaseString(),
-				Namespace: "argo",
-			},
-			Status: pipelinesv1.RunConfigurationStatus{
-				ObservedPipelineVersion: apis.RandomString(),
-				Status: pipelinesv1.Status{
-					ProviderId: pipelinesv1.ProviderAndId{
-						Provider: "stub",
-						Id:       apis.RandomString(),
-					},
-				},
-			},
-		}
+		return withIntegrationTestFields(pipelinesv1.RandomRunConfiguration())
 	}
 
 	DescribeTable("RunConfiguration Workflows", AssertWorkflow[*pipelinesv1.RunConfiguration],
