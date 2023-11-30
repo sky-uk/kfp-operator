03dd60a46b5e173588f54fd2b471d9dd03352575
Ian <ian.graham@sky.uk>
2023-11-30 13:34:52 +0000

    Adding a String() function to RunCompletionEvent so can log without pointer references (#299)

--- argo/common/run_completion.go
+++ argo/common/run_completion.go
@@ -1,5 +1,7 @@
 package common
 
+import "fmt"
+
 const RunCompletionEventName = "run-completion"
 
 type Artifact struct {
@@ -18,12 +20,17 @@ var RunCompletionStatuses = struct {
 }
 
 type RunCompletionEvent struct {
-	Status                RunCompletionStatus    `json:"status"`
-	PipelineName          NamespacedName         `json:"pipelineName"`
+	Status       RunCompletionStatus `json:"status"`
+	PipelineName NamespacedName      `json:"pipelineName"`
 	// Optionally render structs until https://github.com/golang/go/issues/11939 is addressed
-	RunConfigurationName  *NamespacedName        `json:"runConfigurationName,omitempty"`
-	RunName               *NamespacedName        `json:"runName,omitempty"`
-	RunId                 string                 `json:"runId"`
-	ServingModelArtifacts []Artifact             `json:"servingModelArtifacts"`
-	Artifacts             []Artifact             `json:"artifacts,omitempty"`
+	RunConfigurationName  *NamespacedName `json:"runConfigurationName,omitempty"`
+	RunName               *NamespacedName `json:"runName,omitempty"`
+	RunId                 string          `json:"runId"`
+	ServingModelArtifacts []Artifact      `json:"servingModelArtifacts"`
+	Artifacts             []Artifact      `json:"artifacts,omitempty"`
+}
+
+func (sre RunCompletionEvent) String() string {
+	return fmt.Sprintf("{Status:%s PipelineName:%+v RunConfigurationName:%+v RunName:%+v RunId:%s ServingModelArtifacts:%+v Artifacts:%+v}",
+		sre.Status, sre.PipelineName, *sre.RunConfigurationName, *sre.RunName, sre.RunId, sre.ServingModelArtifacts, sre.Artifacts)
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/argoproj/argo-workflows/v3 v3.3.9
 	github.com/docker/distribution v2.7.1+incompatible
 	github.com/google/go-cmp v0.5.9
-	github.com/hashicorp/go-bexpr v0.1.12
 	github.com/onsi/ginkgo/v2 v2.8.0
 	github.com/onsi/gomega v1.25.0
 	github.com/sky-uk/kfp-operator/argo/common v0.0.0-20230602153738-edae94db2513
@@ -57,8 +56,6 @@ require (
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
-	github.com/mitchellh/mapstructure v1.4.3 // indirect
-	github.com/mitchellh/pointerstructure v1.2.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
@@ -71,6 +68,7 @@ require (
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/urfave/cli v1.22.10 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
