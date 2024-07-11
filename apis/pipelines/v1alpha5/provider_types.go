cacab20dbe85f1f7ffdceccc1fc53ea9ea770f8b
Alex Georgousis <alex.georgousis@sky.uk>
2024-07-11 16:42:55 +0100

    New provider helm crd resources

--- apis/pipelines/v1alpha5/provider_types.go
+++ apis/pipelines/v1alpha5/provider_types.go
@@ -2,18 +2,19 @@ package v1alpha5
 
 import (
 	"github.com/sky-uk/kfp-operator/apis"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 type ProviderSpec struct {
 	Image         string `json:"image" yaml:"image"`
 	ExecutionMode string `json:"executionMode" yaml:"executionMode"`
 	// +kubebuilder:validation:Pattern:=`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`
-	ServiceAccount  string            `json:"serviceAccount" yaml:"serviceAccount"`
-	DefaultBeamArgs []apis.NamedValue `json:"defaultBeamArgs,omitempty" yaml:"defaultBeamArgs,omitempty"`
-	// +kubebuilder:validation:Pattern:=`^[a-z]+:\/\/[a-z0-9\-_\/\.]+$`
-	PipelineRootStorage string            `json:"pipelineRootStorage" yaml:"pipelineRootStorage"`
-	Parameters          map[string]string `json:"parameters,omitempty" yaml:"parameters,omitempty"`
+	ServiceAccount      string                           `json:"serviceAccount" yaml:"serviceAccount"`
+	DefaultBeamArgs     []apis.NamedValue                `json:"defaultBeamArgs,omitempty" yaml:"defaultBeamArgs,omitempty"`
+	PipelineRootStorage string                           `json:"pipelineRootStorage" yaml:"pipelineRootStorage"`
+	Parameters          map[string]*apiextensionsv1.JSON `json:"parameters,omitempty" yaml:"parameters,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -44,4 +45,27 @@ func (ps *ProviderStatus) SetSynchronizationState(state apis.SynchronizationStat
 		Status:             ConditionStatusForSynchronizationState(state),
 	}
 	ps.Conditions = ps.Conditions.MergeIntoConditions(condition)
+}
+
+func (p Provider) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      p.Name,
+		Namespace: p.Namespace,
+	}
+}
+
+func (e Provider) GetKind() string {
+	return "provider"
+}
+
+//+kubebuilder:object:root=true
+
+type ProviderList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Provider `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Provider{}, &ProviderList{})
 }
