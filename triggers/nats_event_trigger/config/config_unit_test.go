563cd04c89b776cc3ed009eb0687a258a45f47b6
Ankitha-Krisshnan <ankitha.krisshnan@sky.uk>
2024-10-04 16:36:37 +0100

    Create event-trigger grpc server

---
+++ triggers/nats_event_trigger/config/config_unit_test.go
@@ -0,0 +1,65 @@
+//go:build unit
+
+package nats_event_trigger
+
+import (
+	"os"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/spf13/viper"
+)
+
+func TestNATSEventTriggerConfig(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "NATs Event Trigger Config Unit Suite")
+}
+
+var _ = Context("LoadConfig", func() {
+
+	viper.AddConfigPath(".")
+
+	When("given the default config", func() {
+		It("correctly loads config", func() {
+			expectedConfig := Config{
+				NATSConfig: NATSConfig{
+					Subject: "events",
+					ServerConfig: ServerConfig{
+						Host: "https://localhost",
+						Port: "4222",
+					},
+				},
+				ServerConfig: ServerConfig{
+					Host: "localhost",
+					Port: "50051",
+				},
+			}
+			config, err := LoadConfig()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(config).To(Equal(&expectedConfig))
+		})
+	})
+
+	When("given an enviornment variable", func() {
+		It("correctly override config value", func() {
+			os.Setenv("NATSCONFIG_SERVERCONFIG_PORT", "5000")
+			expectedConfig := Config{
+				NATSConfig: NATSConfig{
+					Subject: "events",
+					ServerConfig: ServerConfig{
+						Host: "https://localhost",
+						Port: "5000",
+					},
+				},
+				ServerConfig: ServerConfig{
+					Host: "localhost",
+					Port: "50051",
+				},
+			}
+			config, err := LoadConfig()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(config).To(Equal(&expectedConfig))
+		})
+	})
+})
