22fc98e64b8a20cb007aaefe2b32978d612023b1
Toby Pinfold <28760528+TobyPinfold@users.noreply.github.com>
2024-10-10 15:18:15 +0100

    Replace http webhook client with grpc

--- controllers/webhook/upstream_unit_test.go
+++ controllers/webhook/upstream_unit_test.go
@@ -4,15 +4,18 @@ package webhook
 
 import (
 	"context"
-	"encoding/json"
+	"fmt"
 	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	config "github.com/sky-uk/kfp-operator/apis/config/v1alpha5"
 	"github.com/sky-uk/kfp-operator/argo/common"
-	"io"
+	pb "github.com/sky-uk/kfp-operator/triggers/nats_event_trigger/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"net"
 	"net/http"
-	"net/http/httptest"
 	"regexp"
 	"strconv"
 )
@@ -24,31 +27,40 @@ func extractHostPort(url string) (string, int) {
 	return matches[1], port
 }
 
-func withHttpWebhook(httpResponseCode int, expectedHeaders http.Header, expectedBody string, f func(upstream ArgoEventWebhook)) func() {
+type stubServer struct {
+	ExpectedResponse *pb.RunCompletionEvent
+	StatusCode       *codes.Code
+	pb.UnimplementedNATSEventTriggerServer
+}
+
+func (s stubServer) ProcessEventFeed(_ context.Context, in *pb.RunCompletionEvent) (*emptypb.Empty, error) {
+	defer GinkgoRecover()
+
+	println("I got a request")
+	Expect(in).To(Equal(s.ExpectedResponse))
+
+	return &emptypb.Empty{}, nil
+}
+
+func withGRPCServer(statusCode codes.Code, expectedResponse *pb.RunCompletionEvent, f func(upstream GrpcNatsTrigger)) func() {
 	return func() {
-		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			defer GinkgoRecover()
+		lis, err := net.Listen("tcp", fmt.Sprintf("%s:%s", "localhost", "4770"))
 
-			Expect(r.URL.String()).To(Equal("/test-path"))
-			for headerName, headerValue := range expectedHeaders {
-				Expect(r.Header.Values(headerName)).To(Equal(headerValue))
-			}
-			content, err := io.ReadAll(r.Body)
-			if err != nil {
-				Expect(err).ToNot(HaveOccurred())
-			}
-			Expect(string(content)).To(ContainSubstring(expectedBody))
-			w.WriteHeader(httpResponseCode)
-		}))
-		defer testServer.Close()
+		if err != nil {
+			panic(fmt.Errorf("failed to listen on port %s: %v", "4770", err))
+		}
 
-		client := testServer.Client()
-		testHost, testPort := extractHostPort(testServer.URL)
-		underTest := ArgoEventWebhook{Upstream: config.Endpoint{
-			Host: testHost,
-			Port: testPort,
-			Path: "/test-path",
-		}, Client: client}
+		s := grpc.NewServer()
+		pb.RegisterNATSEventTriggerServer(s, &stubServer{ExpectedResponse: expectedResponse, StatusCode: &statusCode})
+		if err := s.Serve(lis); err != nil {
+			panic(fmt.Errorf("failed to serve: %v", err))
+		}
+
+		underTest := NewGrpcNatsTrigger(config.Endpoint{
+			Host: "localhost",
+			Port: 4770,
+			Path: "",
+		})
 
 		f(underTest)
 	}
@@ -60,40 +72,15 @@ var _ = Context("call", func() {
 	When("called", func() {
 		rce := common.RunCompletionEvent{}
 		headers := http.Header{"hello": []string{"world", "goodbye"}}
-		bodyBytes, err := json.Marshal(rce)
-		Expect(err).NotTo(HaveOccurred())
-		bodyStr := string(bodyBytes)
 
 		eventData := EventData{
 			Header:             headers,
 			RunCompletionEvent: rce,
 		}
 
-		It("return no error", withHttpWebhook(http.StatusOK, headers, bodyStr, func(underTest ArgoEventWebhook) {
+		It("return no error", withGRPCServer(codes.OK, nil, func(underTest GrpcNatsTrigger) {
 			err := underTest.call(ctx, eventData)
 			Expect(err).NotTo(HaveOccurred())
 		}))
-
-		It("returns internal server error if upstream fails", withHttpWebhook(http.StatusMethodNotAllowed, headers, bodyStr, func(underTest ArgoEventWebhook) {
-			err := underTest.call(ctx, eventData)
-			Expect(err).To(HaveOccurred())
-		}))
-
 	})
-})
-
-var _ = Context("transferHeaders", func() {
-	emptyHeaders := make(http.Header)
-	headers := map[string][]string{"Foo": {"bar"}, "Baz": {"qux"}}
-	DescribeTable("Headers", func(incomingHeaders http.Header, requestHeaders http.Header, expected http.Header) {
-		request := http.Request{Header: requestHeaders}
-		transferHeaders(incomingHeaders, &request)
-		Expect(request.Header).To(Equal(expected))
-	},
-		Entry("No incoming headers or existing request headers", emptyHeaders, emptyHeaders, emptyHeaders),
-		Entry("No incoming headers are transferred to existing request headers", emptyHeaders, headers, headers),
-		Entry("Incoming headers are transferred to empty request headers", headers, emptyHeaders, headers),
-		Entry("Incoming headers are transferred to existing request headers", headers, map[string][]string{"Quux": {"corge"}}, map[string][]string{"Quux": {"corge"}, "Foo": {"bar"}, "Baz": {"qux"}}),
-		Entry("Incoming headers are combined with existing request headers", map[string][]string{"Foo": {"bar"}}, map[string][]string{"Foo": {"baz"}}, map[string][]string{"Foo": {"baz", "bar"}}),
-	)
 })
