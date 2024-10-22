fb8321a84d652306464abe4b56e5d773cbbbf74a
Aidan Dunlop <30293030+aidandunlop@users.noreply.github.com>
2024-10-22 18:05:51 +0100

    add conversion code for experiments and runs

--- apis/pipelines/v1alpha5/experiment_conversion.go
+++ apis/pipelines/v1alpha5/experiment_conversion.go
@@ -1,3 +1,41 @@
 package v1alpha5
 
-func (*Experiment) Hub() {}
+import (
+	hub "github.com/sky-uk/kfp-operator/apis/pipelines/v1alpha6"
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+)
+
+func (src *Experiment) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*hub.Experiment)
+
+	dst.TypeMeta = src.TypeMeta
+	dst.ObjectMeta = src.ObjectMeta
+	dst.Spec.Description = src.Spec.Description
+	dst.Status.SynchronizationState = src.Status.SynchronizationState
+	dst.Status.ProviderId = hub.ProviderAndId{
+		Provider: src.Status.ProviderId.Provider,
+		Id:       src.Status.ProviderId.Id,
+	}
+	dst.Status.ObservedGeneration = src.Status.ObservedGeneration
+	dst.Status.Version = src.Status.Version
+	dst.Status.Conditions = hub.Conditions(src.Status.Conditions)
+
+	return nil
+}
+
+func (dst *Experiment) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*hub.Experiment)
+
+	dst.TypeMeta = src.TypeMeta
+	dst.ObjectMeta = src.ObjectMeta
+	dst.Spec.Description = src.Spec.Description
+	dst.Status.SynchronizationState = src.Status.SynchronizationState
+	dst.Status.ProviderId = ProviderAndId{
+		Provider: src.Status.ProviderId.Provider,
+		Id:       src.Status.ProviderId.Id,
+	}
+	dst.Status.ObservedGeneration = src.Status.ObservedGeneration
+	dst.Status.Version = src.Status.Version
+	dst.Status.Conditions = Conditions(src.Status.Conditions)
+	return nil
+}
--- apis/pipelines/v1alpha5/run_conversion.go
+++ apis/pipelines/v1alpha5/run_conversion.go
@@ -1,3 +1,59 @@
 package v1alpha5
 
-func (*Run) Hub() {}
+import (
+	hub "github.com/sky-uk/kfp-operator/apis/pipelines/v1alpha6"
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+)
+
+func (src *Run) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*hub.Run)
+
+	dst.TypeMeta = src.TypeMeta
+	dst.ObjectMeta = src.ObjectMeta
+	dst.Spec.Pipeline = hub.PipelineIdentifier{
+		Name:    src.Spec.Pipeline.Name,
+		Version: src.Spec.Pipeline.Version,
+	}
+	dst.Spec.ExperimentName = src.Spec.ExperimentName
+	dst.Spec.RuntimeParameters = convertRuntimeParametersToHub(src.Spec.RuntimeParameters)
+	dst.Spec.Artifacts = convertArtifactsToHub(src.Spec.Artifacts)
+
+	dst.Status.ProviderId.Provider = src.Status.ProviderId.Provider
+	dst.Status.ProviderId.Id = src.Status.ProviderId.Id
+
+	dst.Status.SynchronizationState = src.Status.SynchronizationState
+	dst.Status.Version = src.Status.Version
+	dst.Status.ObservedGeneration = src.Status.ObservedGeneration
+	dst.Status.ObservedPipelineVersion = src.Status.ObservedPipelineVersion
+	dst.Status.CompletionState = hub.CompletionState(src.Status.CompletionState)
+	dst.Status.MarkedCompletedAt = src.Status.MarkedCompletedAt
+	dst.Status.Conditions = hub.Conditions(src.Status.Conditions)
+	return nil
+}
+
+func (dst *Run) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*hub.Run)
+
+	dst.TypeMeta = src.TypeMeta
+	dst.ObjectMeta = src.ObjectMeta
+	dst.Spec.Pipeline = PipelineIdentifier{
+		Name:    src.Spec.Pipeline.Name,
+		Version: src.Spec.Pipeline.Version,
+	}
+	dst.Spec.ExperimentName = src.Spec.ExperimentName
+	dst.Spec.RuntimeParameters = convertRuntimeParametersFromHub(src.Spec.RuntimeParameters)
+	dst.Spec.Artifacts = convertArtifactsFromHub(src.Spec.Artifacts)
+
+	dst.Status.ProviderId.Provider = src.Status.ProviderId.Provider
+	dst.Status.ProviderId.Id = src.Status.ProviderId.Id
+
+	dst.Status.SynchronizationState = src.Status.SynchronizationState
+	dst.Status.Version = src.Status.Version
+	dst.Status.ObservedGeneration = src.Status.ObservedGeneration
+	dst.Status.ObservedPipelineVersion = src.Status.ObservedPipelineVersion
+	dst.Status.CompletionState = CompletionState(src.Status.CompletionState)
+	dst.Status.MarkedCompletedAt = src.Status.MarkedCompletedAt
+	dst.Status.Conditions = Conditions(src.Status.Conditions)
+
+	return nil
+}
