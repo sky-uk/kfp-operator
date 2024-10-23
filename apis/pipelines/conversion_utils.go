0140ede8272ab8ea23cd6cceed8302a94bb19ac8
Aidan Dunlop <30293030+aidandunlop@users.noreply.github.com>
2024-10-23 16:08:15 +0100

    add remainder conversion code for runschedule

---
+++ apis/pipelines/conversion_utils.go
@@ -0,0 +1,52 @@
+package pipelines
+
+import (
+	"encoding/json"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +kubebuilder:object:generate=false
+type ConversionRemainder interface {
+	Empty() bool
+	ConversionAnnotation() string
+}
+
+func SetConversionAnnotations(resource metav1.Object, remainders ...ConversionRemainder) error {
+	annotations := resource.GetAnnotations()
+
+	for _, remainder := range remainders {
+		if !remainder.Empty() {
+			remainderJson, err := json.Marshal(remainder)
+			if err != nil {
+				return err
+			}
+
+			if annotations == nil {
+				annotations = map[string]string{}
+			}
+			annotations[remainder.ConversionAnnotation()] = string(remainderJson)
+		}
+	}
+
+	resource.SetAnnotations(annotations)
+
+	return nil
+}
+
+func RetrieveAndUnsetConversionAnnotations(resource metav1.Object, remainders ...ConversionRemainder) error {
+	annotations := resource.GetAnnotations()
+
+	for _, remainder := range remainders {
+		if remainderJson, hasRemainder := annotations[remainder.ConversionAnnotation()]; hasRemainder {
+			err := json.Unmarshal([]byte(remainderJson), remainder)
+			if err != nil {
+				return err
+			}
+
+			delete(annotations, remainder.ConversionAnnotation())
+		}
+	}
+
+	return nil
+}
--- apis/pipelines/v1alpha5/runschedule_conversion.go
+++ apis/pipelines/v1alpha5/runschedule_conversion.go
@@ -1,14 +1,27 @@
 package v1alpha5
 
 import (
+	"github.com/sky-uk/kfp-operator/apis/pipelines"
 	hub "github.com/sky-uk/kfp-operator/apis/pipelines/v1alpha6"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
+func convertScheduleToHubWithRemainder(schedule string, remainder hub.Schedule) (hubSchedule hub.Schedule) {
+	return hub.Schedule{
+		CronExpression: schedule,
+		StartTime:      remainder.StartTime,
+		EndTime:        remainder.EndTime,
+	}
+}
+
 // v1alpha5 -> v1alpha6
 func (src *RunSchedule) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*hub.RunSchedule)
-
+	// get old schedules out from remainder
+	v1alpha6remainder := hub.RunScheduleConversionRemainder{}
+	if err := pipelines.RetrieveAndUnsetConversionAnnotations(src, &v1alpha6remainder); err != nil {
+		return err
+	}
 	dst.TypeMeta = src.TypeMeta
 	dst.ObjectMeta = src.ObjectMeta
 	dst.Spec.Pipeline = hub.PipelineIdentifier{
@@ -18,7 +31,8 @@ func (src *RunSchedule) ConvertTo(dstRaw conversion.Hub) error {
 	dst.Spec.ExperimentName = src.Spec.ExperimentName
 	dst.Spec.RuntimeParameters = src.Spec.RuntimeParameters
 	dst.Spec.Artifacts = convertArtifactsToHub(src.Spec.Artifacts)
-	dst.Spec.Schedule = convertScheduleToHub(src.Spec.Schedule)
+
+	dst.Spec.Schedule = convertScheduleToHubWithRemainder(src.Spec.Schedule, v1alpha6remainder.Schedule)
 	dst.Status = hub.Status{
 		ProviderId: hub.ProviderAndId{
 			Provider: src.Status.ProviderId.Provider,
@@ -36,6 +50,7 @@ func (src *RunSchedule) ConvertTo(dstRaw conversion.Hub) error {
 // v1alpha6 -> v1alpha5
 func (dst *RunSchedule) ConvertFrom(srcRaw conversion.Hub) error {
 	src := srcRaw.(*hub.RunSchedule)
+	v1alpha6remainder := hub.RunScheduleConversionRemainder{}
 
 	dst.TypeMeta = src.TypeMeta
 	dst.ObjectMeta = src.ObjectMeta
@@ -47,6 +62,7 @@ func (dst *RunSchedule) ConvertFrom(srcRaw conversion.Hub) error {
 	dst.Spec.RuntimeParameters = src.Spec.RuntimeParameters
 	dst.Spec.Artifacts = convertArtifactsFromHub(src.Spec.Artifacts)
 	dst.Spec.Schedule = src.Spec.Schedule.CronExpression
+	v1alpha6remainder.Schedule = src.Spec.Schedule
 	dst.Status = Status{
 		ProviderId: ProviderAndId{
 			Provider: src.Status.ProviderId.Provider,
@@ -57,5 +73,6 @@ func (dst *RunSchedule) ConvertFrom(srcRaw conversion.Hub) error {
 		ObservedGeneration:   src.Status.ObservedGeneration,
 		Conditions:           Conditions(src.Status.Conditions),
 	}
-	return nil
+
+	return pipelines.SetConversionAnnotations(dst, &v1alpha6remainder)
 }
