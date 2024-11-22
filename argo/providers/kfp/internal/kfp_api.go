241034d9cdf18339dbc8be931270058ba14f3b18
chr12c <cchhristopher@gmail.com>
2024-11-22 12:08:08 +0000

    use alpha6

--- argo/providers/kfp/internal/kfp_api.go
+++ argo/providers/kfp/internal/kfp_api.go
@@ -1,7 +1,7 @@
 package internal
 
 import (
-	pipelinesv1 "github.com/sky-uk/kfp-operator/apis/pipelines/v1alpha5"
+	pipelinesv1 "github.com/sky-uk/kfp-operator/apis/pipelines/v1alpha6"
 	"github.com/sky-uk/kfp-operator/argo/common"
 )
 
