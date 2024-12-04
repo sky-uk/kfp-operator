d842125f19320b154ee770aa17bd7419cff1a7ef
Toby Pinfold <28760528+TobyPinfold@users.noreply.github.com>
2024-12-04 15:17:37 +0000

    pubsub source unit test cases

--- provider-service/base/pkg/streams/sources/pub_sub_source.go
+++ provider-service/base/pkg/streams/sources/pub_sub_source.go
@@ -8,13 +8,13 @@ import (
 	"github.com/go-logr/logr"
 	"github.com/sky-uk/kfp-operator/argo/common"
 	. "github.com/sky-uk/kfp-operator/provider-service/base/pkg"
-	"os"
 )
 
 type PubSubSource struct {
 	RunsSubscription *pubsub.Subscription
 	Logger           logr.Logger
 	out              chan StreamMessage[string]
+	errOut           chan error
 }
 
 type Resource struct {
@@ -22,8 +22,13 @@ type Resource struct {
 }
 
 type LogEntry struct {
-	Labels   map[string]string `json:"labels"`
-	Resource Resource          `json:"resource"`
+	Resource Resource `json:"resource"`
+}
+
+const PipelineJobLabel = "pipeline_job_id"
+
+func (ps *PubSubSource) ErrorOut() <-chan error {
+	return ps.errOut
 }
 
 func NewPubSubSource(ctx context.Context, project string, subscription string) (*PubSubSource, error) {
@@ -36,6 +41,10 @@ func NewPubSubSource(ctx context.Context, project string, subscription string) (
 	}
 
 	runsSubscription := pubSubClient.Subscription(subscription)
+	exists, err := runsSubscription.Exists(ctx)
+	if err != nil || !exists {
+		return nil, fmt.Errorf("subscription %s does not exist on topic", subscription)
+	}
 
 	pubSubSource := &PubSubSource{
 		Logger: logger,
@@ -45,56 +54,64 @@ func NewPubSubSource(ctx context.Context, project string, subscription string) (
 	go func() {
 		if err := pubSubSource.subscribe(ctx, runsSubscription); err != nil {
 			logger.Error(err, "Failed to subscribe", "subscription", subscription)
-			os.Exit(1)
+			pubSubSource.errOut <- err
+			return
 		}
 	}()
 
 	return pubSubSource, nil
 }
 
-func (es *PubSubSource) Out() <-chan StreamMessage[string] {
-	return es.out
+func (pss *PubSubSource) Out() <-chan StreamMessage[string] {
+	return pss.out
 }
 
-func (es *PubSubSource) subscribe(ctx context.Context, runsSubscription *pubsub.Subscription) error {
-	es.Logger.Info("subscribing to pubsub...")
+func (pss *PubSubSource) subscribe(ctx context.Context, runsSubscription *pubsub.Subscription) error {
+	pss.Logger.Info("subscribing to pubsub...")
 
-	err := runsSubscription.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
-		es.Logger.Info(fmt.Sprintf("message received from Pub/Sub with ID: %s", m.ID))
-		logEntry := LogEntry{}
-		err := json.Unmarshal(m.Data, &logEntry)
+	err := runsSubscription.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
+		pss.Logger.Info(fmt.Sprintf("message received from Pub/Sub with ID: %s", msg.ID))
+
+		pipelineJobId, err := pss.extractPipelineJobId(msg)
 		if err != nil {
-			es.Logger.Error(err, "failed to unmarshal Pub/Sub message")
-			m.Nack()
-			return
-		}
-		es.Logger.Info(fmt.Sprintf("%+v", logEntry))
-
-		pipelineJobId, ok := logEntry.Resource.Labels["pipeline_job_id"]
-		if !ok {
-			es.Logger.Error(err, fmt.Sprintf("logEntry did not contain pipeline_job_id %+v", logEntry))
-			m.Nack()
+			pss.Logger.Error(err, "failed to extract pipeline_job_id from message")
+			msg.Nack()
 			return
 		}
 
 		select {
-		case es.out <- StreamMessage[string]{
+		case pss.out <- StreamMessage[string]{
 			Message: pipelineJobId,
 			OnCompleteHandlers: OnCompleteHandlers{
-				OnSuccessHandler: func() { m.Ack() },
-				OnFailureHandler: func() { m.Nack() },
+				OnSuccessHandler: func() { msg.Ack() },
+				OnFailureHandler: func() { msg.Nack() },
 			},
 		}:
 		case <-ctx.Done():
-			es.Logger.Info("stopped reading from pubsub")
+			pss.Logger.Info("stopped reading from pubsub")
 			return
 		}
 	})
 
 	if err != nil {
-		es.Logger.Error(err, "failed to read from pubsub")
+		pss.Logger.Error(err, "failed to read from pubsub")
 		return err
 	}
 
 	return nil
+}
+
+func (pss *PubSubSource) extractPipelineJobId(msg *pubsub.Message) (string, error) {
+	logEntry := LogEntry{}
+	err := json.Unmarshal(msg.Data, &logEntry)
+	if err != nil {
+		return "", err
+	}
+
+	pipelineJobId, ok := logEntry.Resource.Labels[PipelineJobLabel]
+	if !ok {
+		err := fmt.Errorf("logEntry did not contain %s in %+v", PipelineJobLabel, logEntry)
+		return "", err
+	}
+	return pipelineJobId, nil
 }
