39b2890a90f0a8f5a1907b01d012389840855b97
Alex Georgousis <alex.georgousis@sky.uk>
2024-12-05 17:04:32 +0000

    Add Provider controller

--- apis/pipelines/v1alpha6/test_generators.go
+++ apis/pipelines/v1alpha6/test_generators.go
@@ -44,13 +44,13 @@ func RandomProvider() *Provider {
 			Namespace: "default",
 		},
 		Spec:   RandomProviderSpec(),
-		Status: RandomProviderStatus(),
+		Status: RandomStatus(),
 	}
 }
 
 func RandomProviderSpec() ProviderSpec {
 	randomParameters := make(map[string]*apiextensionsv1.JSON)
-	for key, _ := range RandomMap() {
+	for key := range RandomMap() {
 		randomParameters[key] = &apiextensionsv1.JSON{Raw: []byte(`{"key1": "value1", "key2": 1234}`)}
 	}
 
@@ -69,7 +69,7 @@ func RandomProviderStatus() ProviderStatus {
 }
 
 func RandomConditions() Conditions {
-	return RandomList[metav1.Condition](RandomCondition)
+	return RandomList(RandomCondition)
 }
 
 func RandomCondition() metav1.Condition {
