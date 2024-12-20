f61d769480c372d66f8151cd34dca8d4b42d228f
chr12c <cchhristopher@gmail.com>
2024-12-20 13:55:25 +0000

    fix import

--- provider-service/vai/cmd/main.go
+++ provider-service/vai/cmd/main.go
@@ -8,11 +8,11 @@ import (
 	"github.com/go-resty/resty/v2"
 	"github.com/sky-uk/kfp-operator/argo/common"
 	configLoader "github.com/sky-uk/kfp-operator/provider-service/base/pkg/config"
+	"github.com/sky-uk/kfp-operator/provider-service/base/pkg/server"
 	"github.com/sky-uk/kfp-operator/provider-service/base/pkg/streams/sinks"
 	"github.com/sky-uk/kfp-operator/provider-service/base/pkg/streams/sources"
 	vaiConfig "github.com/sky-uk/kfp-operator/provider-service/vai/internal/config"
 	"github.com/sky-uk/kfp-operator/provider-service/vai/internal/runcompletion"
-	"github.com/sky-uk/kfp-operator/provider-service/base/pkg/server"
 	"go.uber.org/zap/zapcore"
 
 	. "github.com/sky-uk/kfp-operator/provider-service/base/pkg"
