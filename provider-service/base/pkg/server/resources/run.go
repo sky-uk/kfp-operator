a422e307ac3c334c1a5722cee573699e207f1691
Toby Pinfold <28760528+TobyPinfold@users.noreply.github.com>
2024-12-23 14:14:24 +0000

    create base resources within pkg with generic http handler functions for resource actions

---
+++ provider-service/base/pkg/server/resources/run.go
@@ -0,0 +1,20 @@
+package resources
+
+type Run struct{}
+
+func (rs *Run) Name() string {
+	return "run"
+}
+
+func (rs *Run) Create(body []byte) (ResponseBody, error) {
+
+	return ResponseBody{}, nil
+}
+
+func (rs *Run) Update(id string, body []byte) error {
+	return nil
+}
+
+func (rs *Run) Delete(id string) error {
+	return nil
+}
---
+++ provider-service/base/pkg/server/resources/runschedule.go
@@ -0,0 +1,19 @@
+package resources
+
+type RunSchedule struct{}
+
+func (rs *RunSchedule) Name() string {
+	return "runschedule"
+}
+
+func (rs *RunSchedule) Create(body []byte) (ResponseBody, error) {
+	return ResponseBody{}, nil
+}
+
+func (rs *RunSchedule) Update(id string, body []byte) error {
+	return nil
+}
+
+func (rs *RunSchedule) Delete(id string) error {
+	return nil
+}
