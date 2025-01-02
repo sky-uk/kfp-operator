a3313f2c2b18c4cc9ca60da495ecbdb7c5913ba7
Peter Hazell <peter.hazell@sky.uk>
2025-01-02 14:35:31 +0000

    Added service resource for the provider service - server runs and serves requests

--- provider-service/base/pkg/server/server.go
+++ provider-service/base/pkg/server/server.go
@@ -4,17 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/sky-uk/kfp-operator/argo/common"
+	"github.com/sky-uk/kfp-operator/provider-service/base/pkg/config"
 	"github.com/sky-uk/kfp-operator/provider-service/base/pkg/server/resource"
 	"io"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
-	chi "github.com/go-chi/chi/v5"
-	"github.com/sky-uk/kfp-operator/argo/common"
-	"github.com/sky-uk/kfp-operator/provider-service/base/pkg/config"
 )
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
@@ -96,7 +91,6 @@ func New(resources []resource.HttpHandledResource) http.Handler {
 	return mux
 }
 
-// Start starts the HTTP server and gracefully handles shutdown
 func Start(ctx context.Context, cfg config.Server, provider resource.Provider) error {
 	addr := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
 	logger := common.LoggerFromContext(ctx)
@@ -113,28 +107,11 @@ func Start(ctx context.Context, cfg config.Server, provider resource.Provider) e
 	}
 
 	go func() {
-		logger.Info("Starting HTTP server on %s", addr)
+		logger.Info(fmt.Sprintf("Starting HTTP server on %s", addr))
 		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			logger.Error(err, "ListenAndServe", "failed")
 		}
 	}()
-
-	// Wait for termination signal (Ctrl+C, kill, etc.)
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-
-	// Wait for a signal to gracefully shutdown the server
-	<-sigCh
-	logger.Info("Shutting down server...")
-
-	// Graceful shutdown with a timeout
-	shutdownCtx, shutdownCancel := context.WithTimeout(ctx, 5*time.Second)
-	defer shutdownCancel()
-
-	if err := srv.Shutdown(shutdownCtx); err != nil {
-		logger.Error(err, "shutdown", "forced")
-	}
-	logger.Info("Server gracefully stopped")
 
 	return nil
 }
