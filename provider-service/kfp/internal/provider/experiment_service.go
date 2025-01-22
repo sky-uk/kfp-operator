05035858f4927f2c94c3e067253435cbddc11a9c
chr12c <cchhristopher@gmail.com>
2025-01-22 13:15:26 +0000

    implement NewExperimentService

--- provider-service/kfp/internal/provider/experiment_service.go
+++ provider-service/kfp/internal/provider/experiment_service.go
@@ -8,7 +8,7 @@ import (
 	"github.com/sky-uk/kfp-operator/argo/common"
 	"github.com/sky-uk/kfp-operator/provider-service/base/pkg/util"
 	"github.com/sky-uk/kfp-operator/provider-service/kfp/internal/client"
-	"github.com/sky-uk/kfp-operator/provider-service/kfp/internal/config"
+	"google.golang.org/grpc"
 )
 
 type ExperimentService interface {
@@ -29,29 +29,17 @@ type DefaultExperimentService struct {
 
 func NewExperimentService(
 	ctx context.Context,
-	providerConfig config.KfpProviderConfig,
-) (*DefaultExperimentService, error) {
-	// apiUrl, err := url.Parse(providerConfig.Parameters.RestKfpApiUrl)
-	// if err != nil {
-	// 	return nil, err
-	// }
+	conn *grpc.ClientConn,
+) (ExperimentService, error) {
+	if conn == nil {
+		return nil, fmt.Errorf(
+			"no gRPC connection was provided to start experiment service",
+		)
+	}
 
-	// foo := go_client.NewExperimentServiceClient(conn)
-
-	// TODO: GrpcKfpApi needs to instantiate in a way which shares out the
-	// grpc connection so that the ExperimentService also has access.
-	// event flow uses GrpcKfpApi, but not the experiment service - which is
-	// why they are separate.
 	return &DefaultExperimentService{
-		// Client: experiment_client.NewHTTPClientWithConfig(
-		// 	strfmt.NewFormats(), &experiment_client.TransportConfig{
-		// 		Host:     apiUrl.Host,
-		// 		Schemes:  []string{apiUrl.Scheme},
-		// 		BasePath: apiUrl.Path,
-		// 	},
-		// ).ExperimentService,
-
-		ctx: ctx,
+		ctx:    ctx,
+		client: go_client.NewExperimentServiceClient(conn),
 	}, nil
 }
 
