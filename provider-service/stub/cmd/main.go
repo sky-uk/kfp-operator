fbb16d991cc29acd90c234bf4fa19ebe5c736177
chr12c <cchhristopher@gmail.com>
2025-02-07 15:55:45 +0000

    load in the config to main

--- provider-service/stub/cmd/main.go
+++ provider-service/stub/cmd/main.go
@@ -5,30 +5,26 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.com/sky-uk/kfp-operator/argo/common"
-	"github.com/sky-uk/kfp-operator/provider-service/base/pkg/config"
 	"github.com/sky-uk/kfp-operator/provider-service/base/pkg/server"
+	"github.com/sky-uk/kfp-operator/provider-service/stub/internal/config"
 	"github.com/sky-uk/kfp-operator/provider-service/stub/internal/provider"
 	"go.uber.org/zap/zapcore"
 )
 
 func main() {
-
 	logger, err := common.NewLogger(zapcore.InfoLevel)
 	if err != nil {
 		panic(err)
 	}
 
 	ctx := logr.NewContext(context.Background(), logger)
-
 	provider := provider.New(logger)
-
-	// TODO: load in the config
-	cfg := config.Server{
-		Host: "localhost",
-		Port: 8080,
-	}
-	if err = server.Start(ctx, cfg, provider); err != nil {
+	cfg, err := config.LoadConfig(ctx)
+	if err != nil {
 		panic(err)
 	}
 
+	if err = server.Start(ctx, cfg.Server, provider); err != nil {
+		panic(err)
+	}
 }
