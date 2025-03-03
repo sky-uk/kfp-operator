a8f51cbbe6ac0f5bc3dda57a801c8e2da7140501
Ian <ian.graham@sky.uk>
2025-03-03 10:44:45 +0000

    Version length - set to fixed length of 8 characters (#503)

--- version.mk
+++ version.mk
@@ -1,6 +1,6 @@
 ifndef VERSION
 
-VERSION := $(shell (git describe --tags --match 'v[0-9]*\.[0-9]*\.[0-9]*' --dirty 2>/dev/null || echo v0.0.0) | sed 's/^v//')
+VERSION := $(shell (git describe --tags --abbrev=8 --match 'v[0-9]*\.[0-9]*\.[0-9]*' --dirty 2>/dev/null || echo v0.0.0) | sed 's/^v//')
 
 version:
 	@echo ${VERSION}
