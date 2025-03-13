5d6848796c90294a35ebcd457d6960f78e91fcae
Piotr Fras <piotr.fras@sky.uk>
2025-03-13 11:24:56 +0000

    Remove docker-tags from docker-targets

--- docker-targets.mk
+++ docker-targets.mk
@@ -25,6 +25,3 @@ docker-build: GOOS=linux
 docker-build: GOARCH=amd64
 docker-build: build ## Build container image
 	docker build ${DOCKER_BUILD_EXTRA_PARAMS} -t ${IMG} -t ${IMG}:${VERSION} -f Dockerfile .
-
-docker-tag:
-	@echo ${IMG}:${VERSION}
