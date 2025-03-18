c37e44b0130e2ad4d200367603db48023f4f8e79
Piotr Fras <piotr.fras@sky.uk>
2025-03-18 08:49:18 +0000

    Add black python formatter

--- compilers/kfp-sdk/acceptance/test_compiler.py
+++ compilers/kfp-sdk/acceptance/test_compiler.py
@@ -12,18 +12,36 @@ runner = CliRunner()
 @pytest.fixture(scope="session", autouse=True)
 def setup():
     sys.path.append(
-        os.path.join(os.path.dirname(__file__), "..", "..", "..", "docs-gen", "includes", "master", "kfpsdk-quickstart",
-                     "getting_started"))
+        os.path.join(
+            os.path.dirname(__file__),
+            "..",
+            "..",
+            "..",
+            "docs-gen",
+            "includes",
+            "master",
+            "kfpsdk-quickstart",
+            "getting_started",
+        )
+    )
 
 
 def test_compiler__compile(tmp_path):
-    pipeline_config = 'acceptance/pipeline.yaml'
-    provider_config = 'acceptance/provider.yaml'
+    pipeline_config = "acceptance/pipeline.yaml"
+    provider_config = "acceptance/provider.yaml"
     output_file = temp_yaml_file(tmp_path)
 
-    result = runner.invoke(compiler.compile,
-                           ['--pipeline_config', pipeline_config, '--provider_config', provider_config, '--output_file',
-                            str(output_file)])
+    result = runner.invoke(
+        compiler.compile,
+        [
+            "--pipeline_config",
+            pipeline_config,
+            "--provider_config",
+            provider_config,
+            "--output_file",
+            str(output_file),
+        ],
+    )
 
     assert result.exit_code == 0
     assert os.stat(output_file).st_size != 0
