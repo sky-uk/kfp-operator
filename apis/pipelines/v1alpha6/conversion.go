4db5bd24421cc05c37d9d6db71dc41947e658913
chr12c <cchhristopher@gmail.com>
2025-03-18 12:26:40 +0000

    Remove unused providernamespace annotations on v1alpha6

--- apis/pipelines/v1alpha6/conversion.go
+++ apis/pipelines/v1alpha6/conversion.go
@@ -1,18 +1,10 @@
 package v1alpha6
 
 import (
-	"github.com/sky-uk/kfp-operator/apis"
 	"github.com/sky-uk/kfp-operator/argo/common"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var DefaultProviderNamespace string
-
-var ResourceAnnotations = struct {
-	ProviderNamespace string
-}{
-	ProviderNamespace: apis.Group + "/providerNamespace",
-}
 
 func convertProviderTo(
 	provider string,
@@ -82,26 +74,4 @@ func convertProviderFrom5(
 	}
 
 	return provider.Name
-}
-
-func namespaceToProvider(provider string, namespace string) common.NamespacedName {
-	if namespace == "" {
-		namespace = DefaultProviderNamespace
-	}
-
-	return common.NamespacedName{
-		Name:      provider,
-		Namespace: namespace,
-	}
-}
-
-func getProviderNamespaceAnnotation(resource v1.Object) string {
-	if providerNamespace, ok := resource.GetAnnotations()[ResourceAnnotations.ProviderNamespace]; ok {
-		return providerNamespace
-	}
-	return ""
-}
-
-func removeProviderNamespaceAnnotation(resource v1.Object) {
-	delete(resource.GetAnnotations(), ResourceAnnotations.ProviderNamespace)
 }
