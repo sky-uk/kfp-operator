b71a1fc6c8e8d6ec5dac5e34f9b9bf542d8af0bc
Piotr Fras <piotr.fras@sky.uk>
2025-03-18 08:49:18 +0000

    Add black python formatter

--- compilers/kfp-sdk/compiler/compiler.py
+++ compilers/kfp-sdk/compiler/compiler.py
@@ -7,9 +7,13 @@ from kfp import compiler
 
 
 @click.command()
-@click.option('--pipeline_config', help='Pipeline configuration in yaml format', required=True)
-@click.option('--provider_config', help='Provider configuration in yaml format', required=False)
-@click.option('--output_file', help='Output file path', required=True)
+@click.option(
+    "--pipeline_config", help="Pipeline configuration in yaml format", required=True
+)
+@click.option(
+    "--provider_config", help="Provider configuration in yaml format", required=False
+)
+@click.option("--output_file", help="Output file path", required=True)
 def compile(pipeline_config: str, provider_config: str, output_file: str):
     _compile(pipeline_config, output_file)
 
@@ -18,29 +22,38 @@ def _compile(pipeline_config: str, output_file: str):
     """Compiles KFP SDK pipeline into a Kubeflow Pipelines pipeline definition"""
     with open(pipeline_config, "r") as pipeline_stream:
         pipeline_config_contents = yaml.safe_load(pipeline_stream)
-        pipeline_name = sanitise_namespaced_pipeline_name(pipeline_config_contents['name'])
-        click.secho(f'Compiling {pipeline_name} pipeline: {pipeline_config_contents}', fg='green')
+        pipeline_name = sanitise_namespaced_pipeline_name(
+            pipeline_config_contents["name"]
+        )
+        click.secho(
+            f"Compiling {pipeline_name} pipeline: {pipeline_config_contents}",
+            fg="green",
+        )
 
         pipeline_fn = load_fn(pipeline_config_contents)
 
-        compiler.Compiler().compile(pipeline_fn, pipeline_name=pipeline_name, package_path=output_file)
-        click.secho(f'{output_file} compiled', fg='green')
+        compiler.Compiler().compile(
+            pipeline_fn, pipeline_name=pipeline_name, package_path=output_file
+        )
+        click.secho(f"{output_file} compiled", fg="green")
 
 
 def load_fn(pipeline_config_contents: dict):
-    framework_parameters = pipeline_config_contents['framework']['parameters']
-    if 'pipeline' not in framework_parameters:
-        raise KeyError('Missing required framework parameter: [pipeline].')
-    pipeline = framework_parameters['pipeline']
+    framework_parameters = pipeline_config_contents["framework"]["parameters"]
+    if "pipeline" not in framework_parameters:
+        raise KeyError("Missing required framework parameter: [pipeline].")
+    pipeline = framework_parameters["pipeline"]
 
     if "." not in pipeline:
-        raise ValueError(f"Invalid pipeline format: [{pipeline}]. Expected format: 'module_path.function_name'.")
+        raise ValueError(
+            f"Invalid pipeline format: [{pipeline}]. Expected format: 'module_path.function_name'."
+        )
 
-    (module_name, fn_name) = pipeline.rsplit('.', 1)
+    (module_name, fn_name) = pipeline.rsplit(".", 1)
     module = importlib.import_module(module_name)
 
     loaded_module = dir(module)
-    click.secho(f'Loaded module: {loaded_module}', fg='green')
+    click.secho(f"Loaded module: {loaded_module}", fg="green")
 
     fn = getattr(module, fn_name)
 
--- compilers/kfp-sdk/tests/test_compiler.py
+++ compilers/kfp-sdk/tests/test_compiler.py
@@ -11,46 +11,42 @@ def pipeline_fn():
 
 def test_compiler_load_fn():
     pipeline_config_contents = {
-        'framework': {
-            'parameters': {
-                'pipeline': 'test_compiler.pipeline_fn'
-            }
-        }
+        "framework": {"parameters": {"pipeline": "test_compiler.pipeline_fn"}}
     }
     sys.path.append(os.path.dirname(__file__))
     result = compiler.load_fn(pipeline_config_contents)()
     assert result
 
+
 def test_compiler_missing_pipeline_parameter():
-    pipeline_config_contents = {
-        'framework': {
-            'parameters': {}
-        }
-    }
+    pipeline_config_contents = {"framework": {"parameters": {}}}
     sys.path.append(os.path.dirname(__file__))
     with pytest.raises(KeyError) as error:
         compiler.load_fn(pipeline_config_contents)()
 
     assert str(error.value) == "'Missing required framework parameter: [pipeline].'"
 
+
 def test_compiler_invalid_pipeline_format():
-    pipeline_config_contents = {
-        'framework': {
-            'parameters': {
-                'pipeline': 'function'
-            }
-        }
-    }
+    pipeline_config_contents = {"framework": {"parameters": {"pipeline": "function"}}}
     sys.path.append(os.path.dirname(__file__))
     with pytest.raises(ValueError) as error:
         compiler.load_fn(pipeline_config_contents)()
 
-    assert str(error.value) == "Invalid pipeline format: [function]. Expected format: 'module_path.function_name'."
+    assert (
+        str(error.value)
+        == "Invalid pipeline format: [function]. Expected format: 'module_path.function_name'."
+    )
+
 
 def test_sanitise_namespaced_pipeline_name():
-    tests = [('namespace/name', 'namespace-name'), ('test', 'test'), ('/', '-'),
-             ('a/b/c/d/e/f/g/h/', 'a-b-c-d-e-f-g-h-')]
+    tests = [
+        ("namespace/name", "namespace-name"),
+        ("test", "test"),
+        ("/", "-"),
+        ("a/b/c/d/e/f/g/h/", "a-b-c-d-e-f-g-h-"),
+    ]
 
     for input, output in tests:
         sanitised = compiler.sanitise_namespaced_pipeline_name(input)
-        assert sanitised == output, f'Expected {output}, got {sanitised}'
+        assert sanitised == output, f"Expected {output}, got {sanitised}"
