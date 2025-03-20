6c1014dedca0814416d3d5bb60e1f185ea9dbffa
Piotr Fras <piotr.fras@sky.uk>
2025-03-20 08:20:30 +0000

    Override user provided pipeline root with provider configuration

--- compilers/kfp-sdk/compiler/compiler.py
+++ compilers/kfp-sdk/compiler/compiler.py
@@ -1,10 +1,10 @@
 import importlib
 import importlib.util
+import os
 
 import click
 import yaml
 from kfp import compiler
-import os
 
 
 @click.command()
@@ -16,28 +16,38 @@ import os
 )
 @click.option("--output_file", help="Output file path", required=True)
 def compile(pipeline_config: str, provider_config: str, output_file: str):
-    _compile(pipeline_config, output_file)
-
-
-def _compile(pipeline_config: str, output_file: str):
     """Compiles KFP SDK pipeline into a Kubeflow Pipelines pipeline definition"""
-    with open(pipeline_config, "r") as pipeline_stream:
+    with open(pipeline_config, "r") as pipeline_stream, open(
+        provider_config, "r"
+    ) as provider_stream:
         pipeline_config_contents = yaml.safe_load(pipeline_stream)
+        provider_config_contents = yaml.safe_load(provider_stream)
         pipeline_name = sanitise_namespaced_pipeline_name(
             pipeline_config_contents["name"]
         )
+        pipeline_root = get_pipeline_root(provider_config_contents)
         pipeline_environment = pipeline_config_contents.get("env", [])
         click.secho(
-            f"Compiling {pipeline_name} pipeline: {pipeline_config_contents}",
+            f"Compiling {pipeline_name} pipeline with root {pipeline_root}: {pipeline_config_contents}",
             fg="green",
         )
 
         pipeline_fn = load_fn(pipeline_config_contents, pipeline_environment)
 
+        # Override user provided pipeline root with the one from provider configuration.
+        pipeline_fn.pipeline_spec.default_pipeline_root = pipeline_root
+
         compiler.Compiler().compile(
             pipeline_fn, pipeline_name=pipeline_name, package_path=output_file
         )
         click.secho(f"{output_file} compiled", fg="green")
+
+
+def get_pipeline_root(provider_config_contents: dict):
+    if "pipelineRootStorage" in provider_config_contents:
+        return provider_config_contents["pipelineRootStorage"]
+    else:
+        raise KeyError("Missing required pipeline root in provider configuration.")
 
 
 def load_fn(pipeline_config_contents: dict, environment: list):
