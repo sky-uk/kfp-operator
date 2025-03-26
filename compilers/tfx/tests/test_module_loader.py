0dbeabc0894e0e4825466fa1bd30eb7f12a47914
Piotr Fras <piotr.fras@sky.uk>
2025-03-26 18:00:30 +0000

    Improve error message for illegal tfx_components.

--- compilers/tfx/tests/test_module_loader.py
+++ compilers/tfx/tests/test_module_loader.py
@@ -25,3 +25,10 @@ def test_load_fn():
 def test_load_fn_invalid_fn():
     with pytest.raises(Exception) as e_info:
         compiler.load_fn('nonexistent.components_fn', {})()
+
+
+def test_load_fn_invalid_module_path():
+    with pytest.raises(Exception) as e_info:
+        compiler.load_fn('components_fn', {})()
+
+   assert str(e_info.value) == "Invalid pipeline format: [components_fn]. Expected format: 'module_path.function_name'."
