324a5e0c48651a0cf520224e6ed8d01c168d9ff5
Toby Pinfold <28760528+TobyPinfold@users.noreply.github.com>
2025-04-14 16:21:33 +0100

    Remove image from provider spec and add conversion webhook to v1alpha6
    Co-authored-by: chr12c <37987344+chr12c@users.noreply.github.com>

--- apis/pipelines/hub/test_generators.go
+++ apis/pipelines/hub/test_generators.go
@@ -63,6 +63,7 @@ func RandomPipelineSpec(provider common.NamespacedName) PipelineSpec {
 }
 
 func RandomProvider() *Provider {
+
 	return &Provider{
 		TypeMeta: metav1.TypeMeta{},
 		ObjectMeta: metav1.ObjectMeta{
@@ -70,8 +71,13 @@ func RandomProvider() *Provider {
 			Namespace: "default",
 		},
 		Spec:   RandomProviderSpec(),
-		Status: RandomStatus(common.NamespacedName{}),
+		Status: RemoveProvider(RandomStatus(common.NamespacedName{})),
 	}
+}
+
+func RemoveProvider(status Status) Status {
+	status.Provider = ProviderAndId{}
+	return status
 }
 
 func RandomProviderSpec() ProviderSpec {
@@ -81,7 +87,6 @@ func RandomProviderSpec() ProviderSpec {
 	}
 
 	return ProviderSpec{
-		Image:               "kfp-operator-stub-provider",
 		ServiceImage:        "service-image",
 		ExecutionMode:       "none",
 		ServiceAccount:      "default",
