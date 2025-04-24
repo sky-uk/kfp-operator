5e94f1417194607f97ef7fff6db115b299623047
Toby Pinfold <28760528+TobyPinfold@users.noreply.github.com>
2025-04-24 16:47:00 +0100

    fix import

--- controllers/webhook/runcompletion_feed_decoupled_test.go
+++ controllers/webhook/runcompletion_feed_decoupled_test.go
@@ -18,7 +18,6 @@ import (
 	"github.com/sky-uk/kfp-operator/argo/common"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
