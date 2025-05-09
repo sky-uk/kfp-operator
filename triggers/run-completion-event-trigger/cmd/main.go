fe66db9740e868b14af746d82ed79a5db1e579b6
chr12c <cchhristopher@gmail.com>
2025-05-09 17:30:46 +0100

    Revert "Try tel-io"
    This reverts commit 4f55c572a2ff2167066e1759d1657d4406b395d1.

--- triggers/run-completion-event-trigger/cmd/main.go
+++ triggers/run-completion-event-trigger/cmd/main.go
@@ -16,16 +16,15 @@ import (
 	"google.golang.org/grpc"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection"
-
-	grpcmetrics "github.com/tel-io/instrumentation/module/otelgrpc"
-	otelgrpc "go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 )
 
 func main() {
 	logger, err := common.NewLogger(zapcore.InfoLevel)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to create logger: %v", err))
+		logger.Error(err, "Failed to create zap logger")
+		panic(err)
 	}
+
 	ctx := logr.NewContext(context.Background(), logger)
 
 	config, err := configLoader.LoadConfig()
@@ -33,14 +32,6 @@ func main() {
 		logger.Error(err, "Failed to load config file on startup")
 		panic(err)
 	}
-
-	// Start metrics server before anything else
-	metricsShutdown, err := server.MetricsServer{}.Start(ctx, 8081, "runcompletioneventtrigger")
-	if err != nil {
-		logger.Error(err, "Failed to start metrics server")
-		panic(err)
-	}
-	defer metricsShutdown()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%s", config.ServerConfig.Host, config.ServerConfig.Port))
 	if err != nil {
@@ -50,46 +41,42 @@ func main() {
 
 	nc, err := nats.Connect(config.NATSConfig.ServerConfig.ToUrl())
 	if err != nil {
-		logger.Error(err, "Failed to connect to NATS server", "url", config.NATSConfig.ServerConfig.ToUrl())
+		logger.Error(err, "failed to connect to NATS server", "url", config.NATSConfig.ServerConfig.ToUrl())
 		panic(err)
 	}
 	defer nc.Close()
 
 	natsPublisher := publisher.NewNatsPublisher(ctx, nc, config.NATSConfig.Subject)
 
-	grpcMetrics := grpcmetrics.NewServerMetrics(grpcmetrics.WithServerHandledHistogram(true))
-
 	s := grpc.NewServer(
-		grpc.ChainUnaryInterceptor(
-			otelgrpc.UnaryServerInterceptor(),
-			grpcMetrics.UnaryServerInterceptor(),
-			unaryLoggerInterceptor(logger),
-		),
-		grpc.ChainStreamInterceptor(
-			otelgrpc.StreamServerInterceptor(),
-			grpcMetrics.StreamServerInterceptor(),
-		),
+		grpc.UnaryInterceptor(unaryLoggerInterceptor(logger)),
 	)
 
 	pb.RegisterRunCompletionEventTriggerServer(s, &server.Server{Config: config, Publisher: natsPublisher})
+
 	healthpb.RegisterHealthServer(s, &server.HealthServer{HealthCheck: natsPublisher})
+
 	reflection.Register(s)
+
+	server.MetricsServer{}.Start(ctx, 8081, "runcompletioneventtrigger")
 
 	logger.Info("Listening at", "host", config.ServerConfig.Host, "port", config.ServerConfig.Port)
 	if err := s.Serve(lis); err != nil {
-		logger.Error(err, "Failed to serve gRPC service")
+		logger.Error(err, "failed to serve grpc service")
 		panic(err)
 	}
+
 }
 
 func unaryLoggerInterceptor(baseLogger logr.Logger) grpc.UnaryServerInterceptor {
 	return func(
 		ctx context.Context,
-		req interface{},
+		req any,
 		info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler,
-	) (interface{}, error) {
+	) (resp any, err error) {
 		ctx = logr.NewContext(ctx, baseLogger)
+
 		return handler(ctx, req)
 	}
 }
