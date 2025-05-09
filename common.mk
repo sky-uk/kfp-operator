394576e3b99f1103127b83e35408d51272453034
chr12c <cchhristopher@gmail.com>
2025-05-09 11:43:26 +0100

    Update envtest-run to use whatever version of controller-runtime is defined in `go.mod`. In this particular instance it will upgrade from 0.8.3 to 0.15.0

--- common.mk
+++ common.mk
@@ -12,10 +12,12 @@ SHELL = /usr/bin/env bash -o pipefail
 .SHELLFLAGS = -ec
 
 ENVTEST_ASSETS_DIR=$(PROJECT_DIR)/testbin
+CONTROLLER_RUNTIME_VERSION := $(shell go list -m -f '{{.Version}}' sigs.k8s.io/controller-runtime)
 
 define envtest-run
 	mkdir -p ${ENVTEST_ASSETS_DIR}; \
-    test -f ${ENVTEST_ASSETS_DIR}/setup-envtest.sh || curl -sSLo ${ENVTEST_ASSETS_DIR}/setup-envtest.sh https://raw.githubusercontent.com/kubernetes-sigs/controller-runtime/v0.8.3/hack/setup-envtest.sh; \
+    test -f ${ENVTEST_ASSETS_DIR}/setup-envtest.sh || \
+	curl -sSLo ${ENVTEST_ASSETS_DIR}/setup-envtest.sh https://raw.githubusercontent.com/kubernetes-sigs/controller-runtime/$(CONTROLLER_RUNTIME_VERSION)/hack/setup-envtest.sh; \
     source ${ENVTEST_ASSETS_DIR}/setup-envtest.sh; fetch_envtest_tools $(ENVTEST_ASSETS_DIR); setup_envtest_env $(ENVTEST_ASSETS_DIR); $(1)
 endef
 
