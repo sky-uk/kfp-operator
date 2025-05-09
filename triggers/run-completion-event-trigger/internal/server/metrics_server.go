41fb9f4311b6e7453b062b397a580962ef15afdd
chr12c <cchhristopher@gmail.com>
2025-05-09 17:30:46 +0100

    Revert "Try tel-io"
    This reverts commit 4f55c572a2ff2167066e1759d1657d4406b395d1.

--- triggers/run-completion-event-trigger/internal/server/metrics_server.go
+++ triggers/run-completion-event-trigger/internal/server/metrics_server.go
@@ -7,61 +7,77 @@ import (
 	"net"
 	"net/http"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sky-uk/kfp-operator/argo/common"
 )
 
 type MetricsServer struct{}
 
-func (ms MetricsServer) Start(ctx context.Context, port int, serviceName string) (shutdown func(), err error) {
+func (ms MetricsServer) Start(ctx context.Context, port int, serviceName string) error {
 	if port == 0 {
-		return nil, errors.New("metrics.Port must be specified")
+		return errors.New("metrics.Port must be specified")
 	}
 
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return initialiseMetricsServerFromListener(ctx, listener, serviceName)
+	onShutdown, err := initialiseMetricsServerFromListener(ctx, listener, serviceName)
+	if err != nil {
+		return err
+	}
+	defer onShutdown()
+	return nil
 }
 
 func initialiseMetricsServerFromListener(ctx context.Context, listener net.Listener, serviceName string) (func(), error) {
 	logger := common.LoggerFromContext(ctx)
 
+	meterProvider, err := newMeterProvider(serviceName)
+	if err != nil {
+		return nil, err
+	}
+	otel.SetMeterProvider(meterProvider)
+
+	go serveMetrics(ctx, listener)
+
+	return func() {
+		if err := meterProvider.Shutdown(ctx); err != nil {
+			logger.Error(err, "Error shutting down metrics server")
+		}
+	}, nil
+}
+
+func newResource(serviceName string) *resource.Resource {
+	return resource.NewWithAttributes(semconv.SchemaURL,
+		semconv.ServiceName(fmt.Sprintf("provider-service-%s", serviceName)),
+	)
+}
+
+func newMeterProvider(serviceName string) (*metric.MeterProvider, error) {
 	metricExporter, err := prometheus.New()
 	if err != nil {
 		return nil, err
 	}
 
-	provider := metric.NewMeterProvider(
-		metric.WithReader(metricExporter),
-		metric.WithResource(resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceName(fmt.Sprintf("provider-service-%s", serviceName)),
-		)),
-	)
+	meterProvider := metric.NewMeterProvider(metric.WithResource(newResource(serviceName)), metric.WithReader(metricExporter))
+	return meterProvider, nil
+}
 
-	otel.SetMeterProvider(provider)
+func serveMetrics(ctx context.Context, listener net.Listener) {
+	logger := common.LoggerFromContext(ctx)
+	route := "/metrics"
 
-	go func() {
-		mux := http.NewServeMux()
-		mux.Handle("/metrics", promhttp.Handler())
-		logger.Info(fmt.Sprintf("Serving metrics at http://%s/metrics", listener.Addr().String()))
-		if err := http.Serve(listener, mux); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			logger.Error(err, "Metrics HTTP server failed")
-		}
-	}()
-
-	return func() {
-		if err := provider.Shutdown(ctx); err != nil {
-			logger.Error(err, "Error shutting down metrics provider")
-		}
-	}, nil
+	http.Handle(route, promhttp.Handler())
+	if err := http.Serve(listener, nil); err != nil {
+		logger.Error(err, "Metrics serving failed")
+	}
+	logger.Info(fmt.Sprintf("Serving metrics from %s at %s", listener.Addr().String(), route))
 }
