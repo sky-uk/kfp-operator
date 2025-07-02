b594951d53500482bea626191615989cf4bc001f
Toby Pinfold <28760528+TobyPinfold@users.noreply.github.com>
2025-07-02 13:41:37 +0100

    Only populate labels if present
    Co-authored-by: Aidan Dunlop <30293030+aidandunlop@users.noreply.github.com>

--- provider-service/base/pkg/server/trigger/triggers.go
+++ provider-service/base/pkg/server/trigger/triggers.go
@@ -8,8 +8,18 @@ const (
 
 func FromHeaders(headers map[string]string) map[string]string {
 	triggers := map[string]string{}
-	triggers[TriggerType] = headers[TriggerType]
-	triggers[TriggerSource] = headers[TriggerSource]
-	triggers[TriggerSourceNamespace] = headers[TriggerSourceNamespace]
+
+	if triggers[TriggerType] != "" {
+		triggers[TriggerType] = headers[TriggerType]
+	}
+
+	if triggers[TriggerSource] != "" {
+		triggers[TriggerSource] = headers[TriggerSource]
+	}
+
+	if triggers[TriggerSourceNamespace] != "" {
+		triggers[TriggerSourceNamespace] = headers[TriggerSourceNamespace]
+	}
+
 	return triggers
 }
