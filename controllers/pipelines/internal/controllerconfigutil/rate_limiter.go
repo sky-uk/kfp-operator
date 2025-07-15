d700735ae71ce241b55abc9e952486626adb92b4
Toby Pinfold <28760528+TobyPinfold@users.noreply.github.com>
2025-07-15 14:53:27 +0100

    fix comment
    Co-authored-by: chr12c <37987344+chr12c@users.noreply.github.com>

--- controllers/pipelines/internal/controllerconfigutil/rate_limiter.go
+++ controllers/pipelines/internal/controllerconfigutil/rate_limiter.go
@@ -6,5 +6,5 @@ import (
 )
 
 // This rate limiter is used to limit the rate of retry requests.
-// It is set to requeue after 5 minutes for the first 10 retries, and then every 30 minutes thereafter.
+// It is set to requeue after 1 minute for the first 10 retries, and then every 30 minutes thereafter.
 var RateLimiter = workqueue.NewItemFastSlowRateLimiter(1*time.Minute, 30*time.Minute, 10)
