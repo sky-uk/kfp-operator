503469f5a3fc343d80484d977734bf4319815cc2
chr12c <37987344+chr12c@users.noreply.github.com>
2025-09-19 10:23:47 +0100

    Bump controller-runtime to v0.22.1 and controller-gen to v0.19.0 (#645)

--- common.mk
+++ common.mk
@@ -5,20 +5,6 @@ endef
 PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
 export PATH := $(PATH):$(PROJECT_DIR)/bin
 
-# Setting SHELL to bash allows bash commands to be executed by recipes.
-# This is a requirement for 'setup-envtest.sh' in the test target.
-# Options are set to exit when a recipe line exits non-zero or a piped command fails.
-SHELL = /usr/bin/env bash -o pipefail
-.SHELLFLAGS = -ec
-
-ENVTEST_ASSETS_DIR=$(PROJECT_DIR)/testbin
-
-define envtest-run
-	mkdir -p ${ENVTEST_ASSETS_DIR}; \
-    test -f ${ENVTEST_ASSETS_DIR}/setup-envtest.sh || curl -sSLo ${ENVTEST_ASSETS_DIR}/setup-envtest.sh https://raw.githubusercontent.com/kubernetes-sigs/controller-runtime/v0.8.3/hack/setup-envtest.sh; \
-    source ${ENVTEST_ASSETS_DIR}/setup-envtest.sh; fetch_envtest_tools $(ENVTEST_ASSETS_DIR); setup_envtest_env $(ENVTEST_ASSETS_DIR); $(1)
-endef
-
 # go-install will 'go install' any package $2 and install it to $1.
 PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
 export PATH := $(PATH):$(PROJECT_DIR)/bin
