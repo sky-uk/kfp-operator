7c54272117aec4f77313ea51483f8d1acaa473d9
chr12c <cchhristopher@gmail.com>
2025-10-10 15:29:39 +0100

    Fail kfpsdk quickstart

--- docs-gen/includes/master/kfpsdk-quickstart/getting_started/pipeline.py
+++ docs-gen/includes/master/kfpsdk-quickstart/getting_started/pipeline.py
@@ -8,8 +8,7 @@ def add(a: float, b: float) -> float:
 @dsl.component(base_image="python:3.9")
 def write_result(value: float, result: dsl.Output[dsl.Model]):
     """Writes the final result into an artifact file"""
-    with open(result.path, "w") as f:
-        f.write(str(value))
+    raise RuntimeError("Intentional failure for testing")
 
 @dsl.pipeline(
     name="Addition pipeline",
