2ce4bfd8575a63add6a2c83828989a89758b0377
chr12c <cchhristopher@gmail.com>
2025-11-20 17:00:31 +0000

    Define initial controller config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,15 @@
 package config
 
 import (
+	"os"
+	"path/filepath"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
-	"os"
-	"path/filepath"
 )
 
 func K8sClientConfig() (k8sConfig *rest.Config, err error) {
@@ -18,4 +22,49 @@ func K8sClientConfig() (k8sConfig *rest.Config, err error) {
 	}
 
 	return
+}
+
+type OperatorConfig struct {
+	Controller ControllerWrapper `yaml:"controller"`
+	Spec       ConfigSpec        `yaml:"spec"`
+}
+
+type ControllerWrapper struct {
+	GroupKindConcurrency    map[string]int `yaml:"groupKindConcurrency,omitempty"`
+	MaxConcurrentReconciles int            `yaml:"maxConcurrentReconciles,omitempty"`
+	CacheSyncTimeout        time.Duration  `yaml:"cacheSyncTimeout,omitempty"`
+	RecoverPanic            *bool          `yaml:"recoverPanic,omitempty"`
+	NeedLeaderElection      *bool          `yaml:"needLeaderElection,omitempty"`
+}
+
+type ConfigSpec struct {
+	DefaultProvider        string                `yaml:"defaultProvider,omitempty"`
+	DefaultProviderValues  DefaultProviderValues `yaml:"defaultProviderValues,omitempty"`
+	DefaultTfxImage        string                `yaml:"defaultTfxImage,omitempty"`
+	WorkflowTemplatePrefix string                `yaml:"workflowTemplatePrefix,omitempty"`
+	WorkflowNamespace      string                `yaml:"workflowNamespace,omitempty"`
+	Multiversion           bool                  `yaml:"multiversion,omitempty"`
+	DefaultExperiment      string                `yaml:"defaultExperiment,omitempty"`
+	RunCompletionTTL       *metav1.Duration      `yaml:"runCompletionTTL,omitempty"`
+	RunCompletionFeed      ServiceConfiguration  `yaml:"runCompletionFeed,omitempty"`
+}
+
+type DefaultProviderValues struct {
+	Labels               map[string]string  `yaml:"labels,omitempty"`
+	Replicas             int                `yaml:"replicas,omitempty"`
+	PodTemplateSpec      v1.PodTemplateSpec `yaml:"podTemplateSpec,omitempty"`
+	ServiceContainerName string             `yaml:"serviceContainerName,omitempty"`
+	ServicePort          int                `yaml:"servicePort,omitempty"`
+	MetricsPort          int                `yaml:"metricsPort,omitempty"`
+}
+
+type ServiceConfiguration struct {
+	Port      int        `json:"port,omitempty"`
+	Endpoints []Endpoint `json:"endpoints,omitempty"`
+}
+
+type Endpoint struct {
+	Host string `json:"host,omitempty"`
+	Port int    `json:"port,omitempty"`
+	Path string `json:"path,omitempty"`
 }
