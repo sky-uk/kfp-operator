ada0f8f5d8e4e1d2828d15f5297ca9ff99d8f00c
chr12c <cchhristopher@gmail.com>
2025-12-31 11:39:02 +0000

    remove unused NamedValuesToMultiMap

--- controllers/pipelines/internal/workflowfactory/workflow_factory_utils.go
+++ controllers/pipelines/internal/workflowfactory/workflow_factory_utils.go
@@ -11,17 +11,3 @@ func NamedValuesToMap(namedValues []apis.NamedValue) map[string]string {
 
 	return m
 }
-
-func NamedValuesToMultiMap(namedValues []apis.NamedValue) map[string][]string {
-	multimap := make(map[string][]string)
-
-	for _, nv := range namedValues {
-		if _, found := multimap[nv.Name]; !found {
-			multimap[nv.Name] = []string{}
-		}
-
-		multimap[nv.Name] = append(multimap[nv.Name], nv.Value)
-	}
-
-	return multimap
-}
