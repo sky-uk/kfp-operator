ecb2b2529da99ca18de84af8b3b6e95e704724d8
chr12c <cchhristopher@gmail.com>
2026-03-03 19:11:05 +0000

    remove ServingModelArtifact

--- provider-service/kfp/internal/mocks/metadata_store_service_client.go
+++ provider-service/kfp/internal/mocks/metadata_store_service_client.go
@@ -13,32 +13,6 @@ type MockMetadataStoreServiceClient struct {
 	mock.Mock
 }
 
-func (m *MockMetadataStoreServiceClient) GetArtifactType(
-	_ context.Context,
-	in *ml_metadata.GetArtifactTypeRequest,
-	_ ...grpc.CallOption,
-) (*ml_metadata.GetArtifactTypeResponse, error) {
-	args := m.Called(in)
-	var response *ml_metadata.GetArtifactTypeResponse
-	if arg0 := args.Get(0); arg0 != nil {
-		response = arg0.(*ml_metadata.GetArtifactTypeResponse)
-	}
-	return response, args.Error(1)
-}
-
-func (m *MockMetadataStoreServiceClient) GetArtifactsByContext(
-	_ context.Context,
-	in *ml_metadata.GetArtifactsByContextRequest,
-	_ ...grpc.CallOption,
-) (*ml_metadata.GetArtifactsByContextResponse, error) {
-	args := m.Called(in)
-	var response *ml_metadata.GetArtifactsByContextResponse
-	if arg0 := args.Get(0); arg0 != nil {
-		response = arg0.(*ml_metadata.GetArtifactsByContextResponse)
-	}
-	return response, args.Error(1)
-}
-
 func (m *MockMetadataStoreServiceClient) GetContextByTypeAndName(
 	_ context.Context,
 	in *ml_metadata.GetContextByTypeAndNameRequest,
