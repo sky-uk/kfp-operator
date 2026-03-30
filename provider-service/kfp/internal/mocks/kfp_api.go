361df035706579a41c47ab8475792ca57ea4d9ec
chr12c <37987344+chr12c@users.noreply.github.com>
2026-03-30 16:48:34 +0100

    chore(kfp provider): change kfp api mock to be testify-based (#1117)

--- provider-service/kfp/internal/mocks/kfp_api.go
+++ provider-service/kfp/internal/mocks/kfp_api.go
@@ -4,42 +4,23 @@ package mocks
 
 import (
 	"context"
-	"github.com/sky-uk/kfp-operator/pkg/common"
+
 	"github.com/sky-uk/kfp-operator/provider-service/kfp/internal/client/resource"
-	"time"
+	"github.com/stretchr/testify/mock"
 )
 
-// StaticTime Round is used to remove monotonic clock from time.Now() to ensure that the time is compatible with equality checks
-var StaticTime = time.Now().UTC().Round(0)
-
 type MockKfpApi struct {
-	resourceReferences resource.References
-	err                error
+	mock.Mock
 }
 
-func (mka *MockKfpApi) GetResourceReferences(_ context.Context, _ string) (resource.References, error) {
-	return mka.resourceReferences, mka.err
-}
-
-func (mka *MockKfpApi) Reset() {
-	mka.resourceReferences = resource.References{}
-	mka.err = nil
-}
-
-func (mka *MockKfpApi) ReturnResourceReferencesForRun() resource.References {
-	mka.resourceReferences = resource.References{
-		RunConfigurationName: common.RandomNamespacedName(),
-		RunName:              common.RandomNamespacedName(),
-		PipelineName:         common.RandomNamespacedName(),
-		CreatedAt:            &StaticTime,
-		FinishedAt:           &StaticTime,
+func (m *MockKfpApi) GetResourceReferences(
+	_ context.Context,
+	runId string,
+) (resource.References, error) {
+	args := m.Called(runId)
+	var refs resource.References
+	if arg0 := args.Get(0); arg0 != nil {
+		refs = arg0.(resource.References)
 	}
-	mka.err = nil
-
-	return mka.resourceReferences
-}
-
-func (mka *MockKfpApi) Error(err error) {
-	mka.resourceReferences = resource.References{}
-	mka.err = err
+	return refs, args.Error(1)
 }
