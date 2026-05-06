b72ad3708beadcedf41db9e1bcd93621cc50582c
Peter Hazell <peter.hazell@sky.uk>
2026-05-06 15:03:53 +0100

    refactor: apply suggestions

--- minikube.mk
+++ minikube.mk
@@ -2,20 +2,22 @@ MINIKUBE_PROFILE := local-kfp-operator
 MINIKUBE_REGISTRY := localhost:5000/kfp-operator
 MINIKUBE_VERSION := $(shell git describe --tags --match 'v[0-9]*\.[0-9]*\.[0-9]*' | sed 's/^v//')
 MINIKUBE_REGISTRY_PORT = $(shell docker inspect $(MINIKUBE_PROFILE) --format '{{ (index .NetworkSettings.Ports "5000/tcp" 0).HostPort }}')
+MINIKUBE_GOARCH := $(shell go env GOARCH)
 
 ##@ Local development with stub provider (no real training infrastructure needed)
 
 minikube-start: ## Start minikube cluster with registry
 	minikube start -p $(MINIKUBE_PROFILE) --driver=docker --registry-mirror="https://mirror.gcr.io"
 	minikube addons enable registry -p $(MINIKUBE_PROFILE) --images="KubeRegistryProxy=gcr.io/google_containers/kube-registry-proxy:0.4" ## The default gcr.io/k8s-minikube/kube-registry-proxy:0.0.5 isn't available anymore, the real fix is to update to the latest version of minikube
+	minikube ssh -p $(MINIKUBE_PROFILE) "sudo sysctl fs.inotify.max_user_watches=524288 && sudo sysctl fs.inotify.max_user_instances=512"
 
-minikube-install-dependencies: ## Install Argo Workflows, Argo Events, and cert-manager
+minikube-install-dependencies: helm-cmd ## Install Argo Workflows, Argo Events, and cert-manager
 	$(HELM) repo add argo https://argoproj.github.io/argo-helm
 	$(HELM) repo add jetstack https://charts.jetstack.io
 	$(HELM) repo update
-	$(HELM) install argo-workflows argo/argo-workflows -n argo --create-namespace
-	$(HELM) install argo-events argo/argo-events -n argo-events --create-namespace
-	$(HELM) install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true
+	$(HELM) upgrade --install argo-workflows argo/argo-workflows -n argo --create-namespace
+	$(HELM) upgrade --install argo-events argo/argo-events -n argo-events --create-namespace
+	$(HELM) upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true
 	kubectl create namespace kfp-operator-system --dry-run=client -o yaml | kubectl apply -f -
 	@echo "Waiting for cert-manager webhook to be ready..."
 	kubectl rollout status deployment/cert-manager-webhook -n cert-manager
@@ -25,6 +27,7 @@ minikube-install-minio: ## Deploy MinIO for Argo artifact storage
 	kubectl apply -f ./local/minio.yaml
 	kubectl rollout status deployment/minio -n argo
 	@echo "Creating argo-artifacts bucket..."
+	kubectl delete pod minio-setup -n argo --ignore-not-found
 	kubectl run minio-setup --image=minio/mc:latest --restart=Never -n argo --command -- \
 		sh -c 'mc alias set local http://minio:9000 minioadmin minioadmin && mc mb local/argo-artifacts --ignore-existing'
 	kubectl wait --for=jsonpath='{.status.phase}'=Succeeded pod/minio-setup -n argo
@@ -37,16 +40,16 @@ minikube-install-minio: ## Deploy MinIO for Argo artifact storage
 	@echo "MinIO artifact storage configured successfully."
 
 minikube-helm-install: helm-package-operator ./local/values.yaml ## Helm install the operator
-	$(HELM) install -f ./local/values.yaml kfp-operator dist/kfp-operator-$(VERSION).tgz --set containerRegistry=$(MINIKUBE_REGISTRY)
+	$(HELM) upgrade --install -f ./local/values.yaml kfp-operator dist/kfp-operator-$(VERSION).tgz --set containerRegistry=$(MINIKUBE_REGISTRY)
 
 minikube-helm-upgrade: helm-package-operator ./local/values.yaml ## Helm upgrade the operator
 	$(HELM) upgrade -f ./local/values.yaml kfp-operator dist/kfp-operator-$(VERSION).tgz --set containerRegistry=$(MINIKUBE_REGISTRY)
 
 minikube-install-operator: export CONTAINER_REPOSITORIES=localhost:$(MINIKUBE_REGISTRY_PORT)/kfp-operator
 minikube-install-operator: ## Build and push operator + stub images, then helm install
-	$(MAKE) docker-push docker-push-triggers VERSION=$(MINIKUBE_VERSION)
-	$(MAKE) -C provider-service/stub docker-push VERSION=$(MINIKUBE_VERSION)
-	$(MAKE) -C compilers/stub docker-push VERSION=$(MINIKUBE_VERSION)
+	$(MAKE) docker-push docker-push-triggers VERSION=$(MINIKUBE_VERSION) GOARCH=$(MINIKUBE_GOARCH)
+	$(MAKE) -C provider-service/stub docker-push VERSION=$(MINIKUBE_VERSION) GOARCH=$(MINIKUBE_GOARCH)
+	$(MAKE) -C compilers/stub docker-push VERSION=$(MINIKUBE_VERSION) GOARCH=$(MINIKUBE_GOARCH)
 	$(MAKE) minikube-helm-install VERSION=$(MINIKUBE_VERSION) CONTAINER_REPOSITORIES=${CONTAINER_REPOSITORIES}
 
 minikube-apply-provider: ## Apply the stub Provider CR into the cluster
@@ -70,9 +73,9 @@ minikube-up: ## Spin up the full local stack from scratch
 
 minikube-upgrade: export CONTAINER_REPOSITORIES=localhost:$(MINIKUBE_REGISTRY_PORT)/kfp-operator
 minikube-upgrade: ## Rebuild and upgrade operator + stub provider in-place
-	$(MAKE) docker-push docker-push-triggers VERSION=$(MINIKUBE_VERSION)
-	$(MAKE) -C provider-service/stub docker-push VERSION=$(MINIKUBE_VERSION)
-	$(MAKE) -C compilers/stub docker-push VERSION=$(MINIKUBE_VERSION)
+	$(MAKE) docker-push docker-push-triggers VERSION=$(MINIKUBE_VERSION) GOARCH=$(MINIKUBE_GOARCH)
+	$(MAKE) -C provider-service/stub docker-push VERSION=$(MINIKUBE_VERSION) GOARCH=$(MINIKUBE_GOARCH)
+	$(MAKE) -C compilers/stub docker-push VERSION=$(MINIKUBE_VERSION) GOARCH=$(MINIKUBE_GOARCH)
 	$(MAKE) minikube-helm-upgrade VERSION=$(MINIKUBE_VERSION) CONTAINER_REPOSITORIES=${CONTAINER_REPOSITORIES}
 	$(MAKE) minikube-apply-provider
 
